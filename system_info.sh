1b393230c9052e6652da09a016bca262ef1914e5
yennanliue <f339339@gmail.com>
2017-06-24 15:09:12 +0800

    add script brief system status

---
+++ system_info.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# sysinfo_page - A script to produce a system information HTML file
+
+##### Constants
+
+TITLE="System Information for $HOSTNAME"
+RIGHT_NOW=$(date +"%x %r %Z")
+TIME_STAMP="Updated on $RIGHT_NOW by $USER"
+
+##### Functions
+
+system_info()
+{
+    echo "<h2>System release info</h2>"
+    echo "<p>Function not yet implemented</p>"
+
+}   # end of system_info
+
+
+show_uptime()
+{
+    echo "<h2>System uptime</h2>"
+    echo "<pre>"
+    uptime
+    echo "</pre>"
+
+}   # end of show_uptime
+
+
+drive_space()
+{
+    echo "<h2>Filesystem space</h2>"
+    echo "<pre>"
+    df
+    echo "</pre>"
+
+}   # end of drive_space
+
+
+home_space()
+{
+    # Only the superuser can get this information
+
+    if [ "$(id -u)" = "0" ]; then
+        echo "<h2>Home directory space by user</h2>"
+        echo "<pre>"
+        echo "Bytes Directory"
+        du -s /home/* | sort -nr
+        echo "</pre>"
+    fi
+
+}   # end of home_space
+
+
+
+##### Main
+
+cat <<- _EOF_
+***********************************
+
+      $TITLE
+
+      $TIME_STAMP
+      $(system_info)
+      $(show_uptime)
+      $(drive_space)
+      $(home_space)
+***********************************
+_EOF_
