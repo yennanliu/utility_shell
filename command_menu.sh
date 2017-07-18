bf504e3ba68f7bf08bfdf9c84bbf12f5012ab609
yennanliue <f339339@gmail.com>
2017-07-18 16:18:24 +0800

    add user defined functions to command menu

--- command_menu.sh
+++ command_menu.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+### function
+
+
+function to_upper () {
+read -p "plz enter a string : " string 
+#input="$1"
+output=$(tr [a-z] [A-Z]  <<< "$string")
+echo $output
+}
+
+function do_backup {
+tar -czvf $HOME/backup.tgz ${HOME}/bin
+}
+
+function show_cal {
+if [ -x /usr/bin/ncal ] ; then
+command="/usr/bin/ncal -w"
+else
+command="/usr/bin/cal"
+fi
+$command
+}
+
+###
+
+
 while true  
 do
 clear
@@ -8,12 +34,17 @@ echo "a: top"
 echo "b: Display Calendar"
 echo "c: Exit"
 echo "d: date"
+echo "e: to upper"
+echo "f: do backup"
 read -sn1
+#REPLY=$(to_upper "$REPLY")
 case "$REPLY"  in
 	a) top;; 
 	b) cal ;;
 	c) exit 0 ;;
 	d) date ;;
+	e) to_upper $1 ;;
+	f) do_backup ;;
 esac 
 
 read -n1 -p "Press any key to continue"
