3fdedd1cff93513d7c421203c03f0fee89e6220b
yennanliue <f339339@gmail.com>
2018-01-01 17:20:11 +0000

    backup

---
+++ ec2_status_snapshot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+# https://github.com/CaseyLabs/aws-ec2-ebs-automatic-snapshot-bash/blob/master/ebs-snapshot.sh
+
+# https://github.com/amritsingh/ansible_ec2_monitor
+
+
+# dev 
