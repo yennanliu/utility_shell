d54cf126b65582677d77f9d9aa624413a9559c4d
yennanliue <f339339@gmail.com>
2018-01-03 03:59:10 +0000

    rename and add lower string function

--- upper_lower_string.sh
+++ upper_lower_string.sh
@@ -15,20 +15,31 @@ echo  $input
 # upper strings 
 #https://bash.cyberciti.biz/guide/Howto:_convert_string_to_all_uppercase
 
-function capital ()
+function to_capital ()
 {
 input="$1"
 output=$(tr [a-z] [A-Z] <<< "$input")
-echo $output 
+echo "upper case :  $output"
 }
+
+
+function to_lower ()
+{
+input="$1"
+output=$(tr [A-Z] [a-z]  <<< "$input")
+echo "lower case : $output"
+}
+
 
 
 # run 
 
 return_var=$(get $1) 
-echo 'return_var = ' $return_var
+echo 'return_var (input) = ' $return_var
 
-capital $1
+to_capital $1
+to_lower $1
+
 
 
 
