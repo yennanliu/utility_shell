c0ae29599c9b88e21d86bc086005ad8c40044ef7
yennanliu <f339339@gmail.com>
2018-01-04 15:47:30 +0800

    backup

---
+++ create_ec2_log.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/QuickStartEC2Instance.html
+
+
+# cat  /var/log/kern.log
+# terminatee related log 
+	
+# https://stackoverflow.com/questions/726690/who-killed-my-process-and-why
