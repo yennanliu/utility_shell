1a80df1d70ae3976e1540c03ec453e4b36957b94
yennanliue <f339339@gmail.com>
2018-01-12 07:50:55 +0000

    add menu to mysql start/stop command

--- mysql/mysql_server_start_stop.sh
+++ mysql/mysql_server_start_stop.sh
@@ -1,10 +1,48 @@
 #!/bin/sh
 
+# a shell start/stop mysql local server 
+
 # mysql server start 
-mysql.server start
-
-
+#mysql.server start
 # mysql server stop  
+#mysql.server stop
 
-mysql.server stop
+
+function local_mysql_server_start () {
+ echo "mysql.server start"
+ mysql.server start
+ exit 0 ; 
+}
+
+
+function local_mysql_server_stop () {
+ #mysql.server stop
+ echo "mysql.server stop"
+ mysql.server stop
+ exit 0 ; 
+
+}
+
+
+
+while true  
+do
+clear
+echo "Choose an action: a, b or c  "
+echo "a: mysql server start"
+echo "b: mysql server stop"
+echo "c: Exit"
+read -sn1
+#REPLY=$(to_upper "$REPLY")
+case "$REPLY"  in
+	a) local_mysql_server_start;; 
+	b) local_mysql_server_stop ;;
+	c) exit 0 ;;
+esac 
+
+read -n1 -p "Press any key to continue"
+done
+
+
+
 
