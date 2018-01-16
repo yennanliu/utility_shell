636a0e6f1d4abe6ef3ce9be4462d06e6246850e8
yennanliue <f339339@gmail.com>
2018-01-16 07:58:45 +0000

    backup todo script

---
+++ ubuntu_ops/ubuntu_release_memory.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# dev 
+# https://www.tecmint.com/clear-ram-memory-cache-buffer-and-swap-space-on-linux/
+# https://askubuntu.com/questions/155768/how-do-i-clean-or-disable-the-memory-cache
+# https://askubuntu.com/questions/507699/why-does-ubuntu-not-seem-to-release-memory
+# 
