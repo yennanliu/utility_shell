295e62354bc01355093c05bb046a62e50fe1b708
yennanliu <f339339@gmail.com>
2018-01-29 17:37:56 +0000

    fix connection credential

--- ubuntu_ops/ubuntu_2_posgre.sh
+++ ubuntu_ops/ubuntu_2_posgre.sh
@@ -1,18 +1,59 @@
 #!/bin/sh
 
+# specify a password to psql non-interactively?
+# https://stackoverflow.com/questions/6405127/how-do-i-specify-a-password-to-psql-non-interactively
+
+"""
+
+export PGPASSWORD='password'
+psql -h 'server name' -U 'user name' -d 'base name' \
+     -c 'command' (eg. "select * from schema.table")
+
+"""
+
+# ===========================
 
 
-#echo 'db_url : '  $db_url
-#psql  << EOM $db_url EOM 
 
 
 
-echo 'db_url : '  $db_url
-psql $db_url << EOF
-select  * from prc.sdb_members_dev limit 4;
-EOF
+
+"""
+
+### Please export following env variables first in case connect to posgre server 
+
+export PGPASSWORD=<password>
+export host=<hostname>
+export port=<portname>
+export username=<username>
+export dbname=<dbname>
+
+"""
+
 
 
 #psql $host $dbname $username $password $port  << EOF
 #select  * from prc.sdb_members_dev limit 4;
 #EOF
+
+
+
+#export PGPASSWORD='password'
+psql \
+   --host=$host \
+   --port=$port \
+   --username  $username\
+   --dbname=$dbname << EOF
+select  * from prc.sdb_members_dev limit 4;
+EOF
+
+
+
+
+
+
+
+
+
+
+
