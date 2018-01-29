91c68808cfd366e3c2942931a0f49e0230bae8e7
yennanliu <f339339@gmail.com>
2018-01-29 17:38:25 +0000

    backup

---
+++ posgre/insert_to_table.sql
@@ -0,0 +1,17 @@
+
+/*
+INSERT INTO tbl_target
+   (
+      SELECT column_a,
+             column_b,
+             column_c
+        FROM tbl_source
+       WHERE condition
+   );
+
+   
+ */
+
+
+
+ # dev 
