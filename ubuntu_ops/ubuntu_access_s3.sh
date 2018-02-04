d91ee56678ea6d1bccc34dfd471d6c1dfdff2ffe
yennanliue <f339339@gmail.com>
2018-02-04 08:36:51 +0000

    fix comment

--- ubuntu_ops/ubuntu_access_s3.sh
+++ ubuntu_ops/ubuntu_access_s3.sh
@@ -1,12 +1,15 @@
 
 
-# plz install aws command line tool first 
+# install 
 # https://aws.amazon.com/tw/cli/?sc_channel=PS&sc_campaign=Acquisition_UK&sc_publisher=google&sc_medium=command_line_b&sc_content=aws_cli_bmm&sc_detail=%2Bcli%20%2Baws&sc_category=command_line&sc_segment=211476221657&sc_matchtype=b&sc_Country=UK&s_kwcid=AL!4422!3!211476221657!b!!g!!%2Bcli%20%2Baws&ef_id=WjtJQwAADPT4SNmU:20180204080421:s
+# pip install awscli
 
+#-------------
 
-#pip install awscli
+# config
 # set up Access Key ID / Secret Access Key  / region 
 #aws configure 
+
 
 
 # list all buckets 
