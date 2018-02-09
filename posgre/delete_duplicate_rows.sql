40efe3ed58356e5a45cd859f90a9eb59e8f2ece7
yennanliue <f339339@gmail.com>
2018-02-09 06:03:40 +0000

    add more SQL ref

--- posgre/delete_duplicate_rows.sql
+++ posgre/delete_duplicate_rows.sql
@@ -1,6 +1,8 @@
 -- credit http://www.postgresqltutorial.com/how-to-delete-duplicate-rows-in-postgresql/
--- example 
-/*
+
+/* 
+-- example  1 
+
 DELETE
 FROM
     members a
@@ -9,7 +11,49 @@ WHERE
     a.last_event_id < b.last_event_id
     AND a.member_id = b.member_id;
 
+
+--- example  2 
+
+
+SELECT member_id,
+       event_date,
+       event,
+       count(*)
+FROM ana.membership_events
+GROUP BY 1,
+         2,
+         3
+ORDER BY event_date DESC
+
+---
+
+
+SELECT member_id,
+       application_date,
+       count(*)
+FROM ana.members
+GROUP BY 1,
+         2
+ORDER BY application_date DESC
+
+
+---
+
+DELETE
+FROM ana.membership_events a USING ana.membership_events b
+WHERE a.event_id < b.event_id
+  AND a.member_id = b.member_id
+  AND a.event_date = b.event_date
+  AND a.event = b.event
+
+
+---
+
+
+
 */
+
+
 
 
 
