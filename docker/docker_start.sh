a59216294857643fcfa3e721804d097cf90087b4
yennanliu <f339339@gmail.com>
2018-02-14 17:11:50 +0000

    backup docker start shell

---
+++ docker/docker_start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# list all docker images 
+
+docker ps -a 
+
+# run specfic image 
+/*
+
+# list of images 
+
+CONTAINER ID        IMAGE                                COMMAND             CREATED             STATUS                       PORTS               NAMES
+e16f35af2c7f        coorpacademy/docker-pyspark:latest   /bin/bash         29 seconds ago      Up 29 seconds                                    ecstatic_lewin
+679caa89175c        rocker/r-ver:latest                   R                4 minutes ago       Exited (0) 3 minutes ago                        optimistic_murdock
+
+# run commands 
+docker run -it coorpacademy/docker-pyspark:latest /bin/bash
+docker run -it rocker/r-ver:latest R
+
+
+*/
