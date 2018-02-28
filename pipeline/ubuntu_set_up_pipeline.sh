51573e7c42906eafa81877207a420eac5db87a14
yennanliue <f339339@gmail.com>
2018-02-28 07:54:38 +0000

    add pipeline setup help script

---
+++ pipeline/ubuntu_set_up_pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+cd 
+# export env variables 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export db_url='your_db_url'
+# set up digdag 
+source ~/.bashrc
+# to the pipeline directory 
+cd /home/ubuntu/yen_dev/uk_data_team/pipeline
+# push pipeline line project (.dig)
+#digdag push <your_project_name>
+# run digdag server
+digdag server -b 0.0.0.0 -m --task-log ../task_log
+
