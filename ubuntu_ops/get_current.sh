36b2485d33299cd87731e52bc325f2d500a29619
yennanliue <f339339@gmail.com>
2018-03-08 14:29:55 +0000

    add get current time shell

---
+++ ubuntu_ops/get_current.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+current_date_time="`date +%Y%m%d%H%M%S`";
+current_date_time_=`date "+%Y-%m-%d %H:%M:%S"`
+echo 'current_date_time : ' $current_date_time;
+echo 'current_date_time_ : ' $current_date_time_;
