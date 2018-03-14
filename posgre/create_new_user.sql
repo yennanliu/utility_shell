4bb4970cbc05f823e3c5bbebf1a4a927e0a9114d
yennanliu <f339339@gmail.com>
2018-03-14 11:53:35 +0000

    clean code

--- posgre/create_new_user.sql
+++ posgre/create_new_user.sql
@@ -5,10 +5,7 @@
 
 https://www.dbrnd.com/2015/10/postgresql-script-to-create-a-read-only-database-user/
 
-
-
-example 
-
+example :
 
 psql databasehere
 CREATE USER moodle_readonly WITH ENCRYPTED PASSWORD 'blablablapasswordhere';
@@ -20,17 +17,21 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO moodle_reado
 
 */
 
-
 --  Script to Create Read-Only user:
-
+-- please modify user account and password here 
 CREATE ROLE Read_Only_User WITH LOGIN PASSWORD 'Test1234' 
 NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';
 Assign permission to this read only user:
-
+-- please modify the schema name here 
 GRANT CONNECT ON DATABASE YourDatabaseName TO Read_Only_User;
 GRANT USAGE ON SCHEMA public TO Read_Only_User;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO Read_Only_User;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO Read_Only_User;
+
+--
+
+
+
 
 
 
