f4801be2b48cbcf6041a9be7cdc4e3fc3cb908ac
yennanliu <f339339@gmail.com>
2018-03-14 12:31:43 +0000

    create new user account before granting him  access to DBs

--- posgre/posgre_create_readonly_user.sh
+++ posgre/posgre_create_readonly_user.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 
+# ------- create a new read only user  ------- 
+## please modify user account and password here 
+psql \
+   --host=$host \
+   --port=$port \
+   --username  $username\
+   --dbname=$dbname << EOF
+CREATE ROLE test_user WITH LOGIN PASSWORD 'Test1234' 
+NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';
+Assign permission to this read only user:
+EOF
+
 ## declare an array variable
 declare -a schema_list=("ana" "rw" "rpt" "prc")
 
