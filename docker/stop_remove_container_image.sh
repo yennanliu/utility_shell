f4d14f0aa81a6bf1349ca37e0113b6bd99e50c2b
yennanliu <f339339@gmail.com>
2018-03-15 14:46:12 +0000

    shell remove docker image/container

---
+++ docker/stop_remove_container_image.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+
+# credit 
+#http://blog.baudson.de/blog/stop-and-remove-all-docker-containers-and-images
+
+
+# List all containers (only IDs)
+docker ps -aq
+
+# Stop all running containers
+docker stop $(docker ps -aq)
+# Remove all containers
+docker rm $(docker ps -aq)
+# Remove all images
+docker rmi $(docker images -q)
