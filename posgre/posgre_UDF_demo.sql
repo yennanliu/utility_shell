56ed84839116343b52a9f7df0fcc4dc19d90acfd
yennanliu <f339339@gmail.com>
2018-03-21 18:13:40 +0000

    add more pgSQL ref

--- posgre/posgre_UDF_demo.sql
+++ posgre/posgre_UDF_demo.sql
@@ -4,6 +4,18 @@
 -- Posgre UDF 
 -- https://www.tutorialspoint.com/postgresql/postgresql_functions.htm
 
+-- PL/pgSQL turtorial 
+-- https://w3resource.com/PostgreSQL/pl-pgsql-declarations.php
+-- http://www.imada.sdu.dk/~jamik/db2-12/material/39-PL-pgSQL.pdf
+
+
+-- Posgre cursor 
+-- http://www.postgresqltutorial.com/plpgsql-cursor/
+
+
+
+
+
 
 
 -- for loop example 
