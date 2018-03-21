9bfb0ccb997b105453e20f9da4ad11d350fe81bb
yennanliu <f339339@gmail.com>
2018-03-21 17:08:52 +0000

    backup for loop example in comments

--- posgre/posgre_UDF_demo.sql
+++ posgre/posgre_UDF_demo.sql
@@ -1,8 +1,43 @@
+/*
 
 
 -- Posgre UDF 
 -- https://www.tutorialspoint.com/postgresql/postgresql_functions.htm
 
+
+
+-- for loop example 
+https://dba.stackexchange.com/questions/89643/return-the-id-after-insert-or-select
+
+###
+
+CREATE OR REPLACE FUNCTION f_email_insel(_email text, OUT email_id int) AS
+$func$
+BEGIN
+
+LOOP
+   BEGIN  -- start inner block inside loop to handle possible exception
+
+   SELECT INTO email_id  m.email_id FROM mailing_list m WHERE m.email = _email;
+
+   IF NOT FOUND THEN
+      INSERT INTO mailing_list (email) VALUES (_email)
+      RETURNING mailing_list.email_id INTO email_id;
+   END IF;
+
+   EXCEPTION WHEN UNIQUE_VIOLATION THEN     -- inserted in concurrent session.
+      RAISE NOTICE 'It actually happened!'; -- hardly ever happens
+   END;
+
+   EXIT WHEN email_id IS NOT NULL;          -- else keep looping
+END LOOP;
+
+END
+$func$ LANGUAGE plpgsql;
+
+### 
+
+*/
 CREATE OR REPLACE FUNCTION upsert_tableName() RETURNS VOID AS $$ 
     DECLARE 
     BEGIN 
@@ -18,3 +53,8 @@ CREATE OR REPLACE FUNCTION upsert_tableName() RETURNS VOID AS $$
  -- execute UDF 
 
 select upsert_tableName(); 
+
+
+
+
+
