12be46fc186ae8cda6b6a829afe3c4f00d740862
yennanliu <f339339@gmail.com>
2018-04-18 13:18:40 +0100

    add step comments  encrypt part

--- ubuntu_ops/ubuntu_sf_vcc_gpg.sh
+++ ubuntu_ops/ubuntu_sf_vcc_gpg.sh
@@ -7,11 +7,17 @@ echo $current_date
 echo $db_url 
 echo $User
 echo $Host 
+echo $recipient_mail 
 
+echo 'query DB....'
 psql $db_url -c"\COPY (SELECT * FROM rw.job_health limit 10 ) To 'test$current_date.csv' WITH CSV HEADER DELIMITER ','"
 file_name="test$current_date.csv"
 echo $file_name
+echo 'encrypt the data....'
+gpg --encrypt --recipient $recipient_mail  $file_name
+echo 'to sftp....'
 sshpass -p $password sftp -oport=22 $User@$Host << !
-put $file_name /Import
+put $file_name.gpg /Import
 !
 rm $file_name
+rm $file_name.gpg
