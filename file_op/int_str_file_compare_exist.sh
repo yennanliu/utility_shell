b9e77364671d9f4ebd05f34d0eb15130017d90a1
yennanliue <f339339@gmail.com>
2018-05-06 08:59:43 +0100

    create files relatvie op scripts collection

---
+++ file_op/int_str_file_compare_exist.sh
@@ -0,0 +1,47 @@
+
+
+#!/bin/sh
+
+echo '-------------'
+echo '#### compare between integer  ####'
+echo '0: true , 1 : false'
+
+[ 2 -gt 1 ]
+echo $?
+echo '-------------'
+[ 1 -gt 2 ]
+echo $?
+echo '-------------'
+echo '#### compare between string  ####'
+x=100
+y=80
+[ $x -gt $y ]
+echo $?
+echo '-------------'
+echo '#### integer operation  ####'
+x=100
+y=80
+let "y+=200"
+[ $x -gt $y ]
+echo $?
+echo '-------------'
+echo '#### check if string is null ####'
+mystring=""
+echo $mystring
+[ -z $mystring ]
+echo $?
+echo '-------------'
+echo '#### check if file exist ####'
+[ -e /Users/yennanliu/utility_shell/README.md ]
+echo $?
+echo '-------------'
+echo '#### check if directory exist ####'
+[ -e /Users/yennanliu/utility_shell/docker ]
+echo $?
+echo '-------------'
+
+
+
+
+
+
