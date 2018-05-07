e4ba4e682531fea1594c1e88a8f4f81bc6a0f390
yennanliue <f339339@gmail.com>
2018-05-07 09:34:11 +0100

    add copy multiple files help script

---
+++ file_op/copy_ls_files.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+
+<<COMMENT1
+"""
+cp `ls /Users/yennanliu/spark/jars/scala*`  /Users/yennanliu/utility_Java
+"""
+COMMENT1
+
+
+
+
+
+cp `ls <need_to_copy_file_directory>`  <target_directory>
