5c4cd6fccd720d24226712ce7061f89656360a80
yennanliue <f339339@gmail.com>
2018-05-10 10:32:16 +0100

    add help command start/stop mongo

---
+++ mongo/mongo_start_stop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+
+# start mongo server locally 
+mongod 
+
+# stop mongo server locally 
+# https://stackoverflow.com/questions/11774887/how-to-stop-mongo-db-in-one-command
+#Kill the process by $ kill <PID> (the Mongo docs have more info on this) 
