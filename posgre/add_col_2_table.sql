f541f9387f528e93d976edefd83e8399469b0cba
yennanliu <f339339@gmail.com>
2018-07-09 15:11:29 +0100

    add sql command add new cols to exist table

---
+++ posgre/add_col_2_table.sql
@@ -0,0 +1,13 @@
+
+-- http://www.postgresqltutorial.com/postgresql-add-column/
+
+
+-- example 
+
+ 
+ALTER TABLE ana.member_profile 
+ADD COLUMN phone5  VARCHAR ,  
+ADD COLUMN phone6  VARCHAR , 
+ADD COLUMN phone7  VARCHAR ; 
+
+
