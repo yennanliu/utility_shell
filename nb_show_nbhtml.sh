542d90d465e4cc68462d25bf8b76ad3df0ad0201
yennanliue <f339339@gmail.com>
2018-07-14 14:55:36 +0100

    rename file

---
+++ nb_show_nbhtml.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+#http://nbviewer.jupyter.org/github/nicolasfauchereau/Python_NIWA_Wellington/blob/master/notebooks/IPython_notebook.html
+
+# !ipython nbconvert {name}.ipynb --to html
+# display(HTML("<a href='{name}.html' target='_blank'> {name}.html </a>".format(name=name)))
+# !open ./{name}.html
