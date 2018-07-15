1778aab912e97cd79463ceace7bf9a0e65b47503
yennanliue <f339339@gmail.com>
2018-07-15 18:56:58 +0100

    add list table command

--- posgre/psql_shell_command.sql
+++ posgre/psql_shell_command.sql
@@ -1,5 +1,6 @@
 
 
+# https://dba.stackexchange.com/questions/1285/how-do-i-list-all-databases-and-tables-using-psql
 
 
 -- show DB
@@ -7,5 +8,8 @@
 \list
 
 
+
+-- show  tabl e
+\d+ 
 -- show Role
 \du
