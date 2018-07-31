f5b2b42fb3996052b5a5e21ac2de058dcb0740a2
yennanliue <f339339@gmail.com>
2018-07-31 06:52:28 +0100

    add shell count input_arguments

---
+++ archive/num_input_arguments.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+
+# V1 
+
+echo "Number of arguments $#   (V1)"
+while [ -n "$1" ] && [ -n "$2" ]  && [ -n "$3" ] ; do 
+	echo "$1 $2 $3"
+	shift 3  # shift 3 arguments. i.e. 1,2,3 -> 4,5,6....
+done 
+
+if [ -n "$1" ] &&   [ -n "$2" ]
+then echo "$1 $2"
+else 
+echo $1
+fi   
+ 
+
+
+
+
+
+# V2 
+echo "Number of arguments $#   (V2)"
+while [ -n "$1" ] && [ -n "$2" ]  && [ -n "$3" ] ; do 
+	echo "$1 $2 $3"
+	shift 3  # shift 3 arguments. i.e. 1,2,3 -> 4,5,6....
+done 
+
+echo $@  # if not input at least 3 arguments, just print all input arguments instead 
+ 
+
+
+
+
+
+
+
+
