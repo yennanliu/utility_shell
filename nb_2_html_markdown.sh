101985271fccca3c44ee6028fd68314cc5d2bca3
yennanliue <f339339@gmail.com>
2018-08-10 01:34:12 +0100

    add nb to markdown command and rename the script

--- nb_2_html_markdown.sh
+++ nb_2_html_markdown.sh
@@ -6,4 +6,9 @@
 # todo :
 # let user can input notebook name 
 
+# nb to html 
 jupyter nbconvert --to html <your_notebook>.ipynb
+
+
+# nb to markdown 
+jupyter nbconvert --to markdown <your_notebook>.ipynb
