9460e821b26a2cccd227476b494579e21042db9d
yennanliue <f339339@gmail.com>
2018-08-24 14:31:40 +0100

    add shell check file types

---
+++ file_op/check_file_directory.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+
+for name in `ls "$1"` ; do
+
+if 
+	[ -f "$1/$name" ] ; then  # check if is a normal file 
+	echo "$1/$name (is a file)"
+elif [[ -d "$1/$name" ]]; then # check if is a directory 
+	echo "$1/$name (is a directory)"
+
+else 
+	echo  "$1/$name (is others )"
+
+fi 
+done 
