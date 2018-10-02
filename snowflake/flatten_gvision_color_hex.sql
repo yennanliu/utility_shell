20eeb6c1a67e5028463c73a6754a57f8f9e4dadc
yennanliu <f339339@gmail.com>
2018-10-02 12:45:37 +0100

    update sql

--- snowflake/flatten_gvision_color_hex.sql
+++ snowflake/flatten_gvision_color_hex.sql
@@ -1,11 +1,25 @@
 
 
-
--- 1)
-SELECT value:"json":"url"::varchar,
-                    value:"json":"cropHintsAnnotation"
-FROM YENNANL_DEVELOPMENT_DATAWAREHOUSE.ADYEN.YEN_DEV31 "a",
-     LATERAL FLATTEN(INPUT => "a"."json", OUTER => TRUE) b
+-- 0)
+select * from YENNANL_DEVELOPMENT_DATAWAREHOUSE.ADYEN.YEN_DEV31;
 
 
 -- 2)
+SELECT value:"json":"url"::varchar AS url,
+                    value:"json":"cropHintsAnnotation" AS json_,
+                                 value:"json":"cropHintsAnnotation":"cropHints" AS cropHints
+FROM YENNANL_DEVELOPMENT_DATAWAREHOUSE.ADYEN.YEN_DEV31 "a",
+     LATERAL FLATTEN(INPUT => "a"."json", OUTER => TRUE) b
+
+-- 3) 
+SELECT value:"json":"url"::varchar AS url,
+                    value:"json":"cropHintsAnnotation" AS json_,
+                                 value:"json":"cropHintsAnnotation":"cropHints" AS cropHints
+FROM YENNANL_DEVELOPMENT_DATAWAREHOUSE.ADYEN.YEN_DEV31 "a",
+     LATERAL FLATTEN(INPUT => "a"."json", OUTER => TRUE) "b",
+             LATERAL FLATTEN(INPUT => "b":"boundingPoly", OUTER => TRUE) "c"
+
+
+
+
+
