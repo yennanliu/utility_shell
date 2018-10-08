f9438c361f96c791b68a85bc3da71a1eaf803528
yennanliu <f339339@gmail.com>
2018-10-08 10:21:48 +0100

    add zip/unzip demo command

---
+++ file_op/zip_unzip_file.sh
@@ -0,0 +1,11 @@
+
+
+# https://coolestguidesontheplanet.com/how-to-compress-and-uncompress-files-and-folders-in-os-x-lion-10-7-using-terminal/
+
+
+
+# compress 
+zip -r <zip_file_name>.zip <folder_to_compress>
+
+# uncompress 
+unzip <archive_name>.zip
