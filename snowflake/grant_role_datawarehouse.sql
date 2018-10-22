a2959a2f789f218148d6e30c9842e9e93186404f
yennanliu <f339339@gmail.com>
2018-10-22 16:24:00 +0100

    add grant user query table command

--- snowflake/grant_role_datawarehouse.sql
+++ snowflake/grant_role_datawarehouse.sql
@@ -1,4 +1,10 @@
 
 #https://docs.snowflake.net/manuals/sql-reference/sql/grant-privilege-share.html
 
+# grant user to see the db 
 GRANT USAGE ON DATABASE your_datawarehouse_name_DATAWAREHOUSE TO ROLE DATA_SCIENCE;
+
+# grant user to select under schema 
+GRANT SELECT ON ALL TABLES IN SCHEMA your_datawarehouse_name_DATAWAREHOUSE.IMPACT TO ROLE DATA_SCIENCE;
+
+
