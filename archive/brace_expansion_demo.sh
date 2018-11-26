c14f193fe8ca67dc110be961ea54ccb77c61abcc
yennanliue <f339339@gmail.com>
2018-11-26 07:29:28 +0000

    add brace_expansion / echo_func_value demo example

---
+++ archive/brace_expansion_demo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# https://devhints.io/bash
+
+
+
+echo {A,B}     # A  B 
+echo {A,B}.js  # A.js B.js
+echo {1..5}    # 1 2 4 5 6 
+echo {1..5}.js # 1.js 2.js 3.js 4.js 5.js
+echo {a..z}    # a b c d e f g h i j k l m n o p q r s t u v w x y z
---
+++ archive/echo_func_value.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# https://devhints.io/bash
+
+get_name() {
+  echo "Jim"
+}
+
+echo "Hi there, This is $(get_name)"
