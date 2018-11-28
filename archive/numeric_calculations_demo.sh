4b2c8c3f3981baec495da98f1858c78a80789e42
yennanliue <f339339@gmail.com>
2018-11-28 07:33:48 +0000

    add numeric_calculate shell demo

---
+++ archive/numeric_calculations_demo.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# https://devhints.io/bash
+
+: '
+
+### Numeric calculations
+$((a + 200))      # Add 200 to $a
+$((RANDOM%=200))  # Random number 0..200
+
+'
+
+
+# -----------------------------------
+echo 'DEMO 1)'
+
+mynumber=100
+echo 'mynumber = ' mynumber
+echo 'mynumber + 100 =' $((mynumber+100))
+echo 'mynumber - 10 ='  $((mynumber-10))
+echo 'mynumber * 10 ='  $((mynumber*10))
+echo 'mynumber / 50 ='  $((mynumber/50))
+
+
+# -----------------------------------
+echo 'DEMO 2)'
+
+echo 'Random number 0..200 =' $((RANDOM%=200))  # Random number 0..200
+
+
+
