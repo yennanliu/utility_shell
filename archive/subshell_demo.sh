8603f43fdeee926d5bd5f3737b95b9ca76424255
yennanliue <f339339@gmail.com>
2018-11-28 07:57:47 +0000

    add subshell command demo

---
+++ archive/subshell_demo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# https://devhints.io/bash
+
+
+
+
+: '
+### EXAMPLE 
+(cd somedir; echo 'I am now in $PWD')
+pwd # still in first directory
+'
+
+
+
+
+
+# -----------------------------------
+echo 'DEMO 1)'
+
+(cd  && ls  && pwd && echo "I am here  (subshell):  $PWD" )
+echo "I am here  (shell):  $PWD"
