f55b9391d206edce47c5238567b8c2adda34699f
yennanliue <f339339@gmail.com>
2018-11-28 07:25:12 +0000

    backup code

---
+++ archive/options_demo.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# https://devhints.io/bash
+
+: '
+### Options
+set -o noclobber  # Avoid overlay files (echo "hi" > foo)
+set -o errexit    # Used to exit upon error, avoiding cascading errors
+set -o pipefail   # Unveils hidden failures
+set -o nounset    # Exposes unset variables
+
+### Glob options
+set -o nullglob    # Non-matching globs are removed  ('*.foo' => '')
+set -o failglob    # Non-matching globs throw errors
+set -o nocaseglob  # Case insensitive globs
+set -o globdots    # Wildcards match dotfiles ("*.sh" => ".foo.sh")
+set -o globstar    # Allow ** for recursive matches ('lib/**/*.rb' => 'lib/a/b/c.rb')
+'
+
+
+# dev 
