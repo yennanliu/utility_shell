853955b16a4a24e4a3b20f98a1b6dd0e41bb6bdf
yennanliue <f339339@gmail.com>
2018-11-28 08:39:04 +0000

    backup printf demo

---
+++ archive/printf_demo.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# https://robots.thoughtbot.com/input-output-redirection-in-the-shell
+
+
+: '
+### Printf demo 
+
+printf 'Hello %s, I am  %s' Sven Olga
+
+'
+
+# -----------------------------------
+echo 'DEMO 1)'
+
+printf 'Hello %s, I am  %s ' OLIVIA YEN
+
+
+# -----------------------------------
+echo 'DEMO 2)'
+
+printf 'SELECT * FROM %s LIMIT 10 ' ETL_LOG
+
+
+# -----------------------------------
+echo 'DEMO 3)'
+
+#printf 'TODAY IS : %s' $date
+# dev 
+
