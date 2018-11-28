cb9ef7b9528c408b230ba80dfcd72f7d784e10f2
yennanliue <f339339@gmail.com>
2018-11-28 07:52:28 +0000

    add heredoc_demo shell

---
+++ archive/heredoc_demo.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# https://devhints.io/bash
+
+
+
+
+
+
+# -----------------------------------
+echo 'DEMO 1)'
+cat <<END
+hello world
+END
+
+
+# -----------------------------------
+echo 'DEMO 2)'
+#ls <<END
+#cd  && pwd 
+#END
+
+# dev 
