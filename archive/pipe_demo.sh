d7125befe4155dd541af44b4c8f4232db3c5f174
yennanliue <f339339@gmail.com>
2018-11-28 08:32:54 +0000

    fix layout

--- archive/pipe_demo.sh
+++ archive/pipe_demo.sh
@@ -3,7 +3,6 @@
 # https://robots.thoughtbot.com/input-output-redirection-in-the-shell
 
 
-
 : '
 ### Pipes demo 
 
@@ -15,8 +14,6 @@ hi there
 $ echo "hello there" | sed "s/hello/hi/" | sed "s/there/robots/"
 hi robots
 '
-
-
 
 # -----------------------------------
 echo 'DEMO 1)'
@@ -32,7 +29,6 @@ echo "hello world"
 echo "hello world" | sed "s/hello/hiya/"  | sed "s/world/EARTH/"  # replace hello with hiya then replace world with EARTH
 
 
-
 # -----------------------------------
 echo 'DEMO 3)'
 
@@ -41,23 +37,10 @@ echo 'A =' $A
 echo  'A =' $A | echo 'A + 100 =' $((A+100)) |  echo '(A + 100)*3 =' $(((A+100)*3))
 
 
-
 # -----------------------------------
 echo 'DEMO 4)'
 
 #A=100
 #echo 'A =' $A
 #A=$( echo 'A+100' | bc) | echo $A
-
-
-
-
-
-
-
-
-
-
-
-
 
