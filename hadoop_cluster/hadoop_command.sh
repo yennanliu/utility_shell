90b80562230ca523d381d0f6d637a26f6bec11b8
yennanliue <f339339@gmail.com>
2019-01-10 08:15:39 +0000

    add hadoop help command

---
+++ hadoop_cluster/hadoop_command.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+
+#################################################################
+# USEFUL COMMAND RUNNING OP ON HADOOP CLUSTER MACHINE 
+#################################################################
+
+<<COMMENT1
+
+COMMENT1
+
+
+# login 
+chmod 600 <ssh_key>
+ssh -i <ssh_key> <user>@<hadoop_ip>
+
+# copy files to local 
+scp -i  <ssh_key> -r <ssh_key> <user>@<hadoop_ip> . 
+
+
+
+
+
+
+
+
+
+
+
+
+
