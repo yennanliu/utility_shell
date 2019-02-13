6fb8f4a9dabb870035b6816bc05e81edf903a2dc
yennanliu <f339339@gmail.com>
2019-02-13 12:32:52 +0000

    add command dump all s3 json to db

---
+++ snowflake/dump_all_s3_json_snowflake.sh
@@ -0,0 +1,22 @@
+
+
+#!/bin/sh
+
+#################################################################
+# COMMAND COPY ALL FILTED S3 JSON FILES TO DB 
+#################################################################
+
+
+
+
+COPY INTO <dw_name>.<schema_name>.<table_name>
+FROM s3://<s3_bucket_name>
+CREDENTIALS=(
+    AWS_KEY_ID=''
+    AWS_SECRET_KEY=''
+)
+FILE_FORMAT=(
+    VALIDATE_UTF8=FALSE
+    TYPE=JSON
+)
+PATTERN='.*<s3_file_name>.*[.]json[.]gz'
