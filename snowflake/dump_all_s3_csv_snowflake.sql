4d545f3b58d26a78749eec470119f201bf9778e0
yennanliu <f339339@gmail.com>
2019-02-13 12:30:10 +0000

    add command dump all s3 csv to db

---
+++ snowflake/dump_all_s3_csv_snowflake.sql
@@ -0,0 +1,25 @@
+
+
+#!/bin/sh
+
+#################################################################
+# COMMAND COPY ALL FILTED S3 FILES TO DB 
+#################################################################
+
+
+
+
+COPY INTO <dw_name>.<schema_name>.<table_name>
+FROM s3://<s3_bucket_name>
+CREDENTIALS=(
+    AWS_KEY_ID=''
+    AWS_SECRET_KEY=''
+)
+FILE_FORMAT=(
+    VALIDATE_UTF8=FALSE
+    TYPE=CSV
+    FIELD_OPTIONALLY_ENCLOSED_BY='"'
+    ERROR_ON_COLUMN_COUNT_MISMATCH=True
+    SKIP_HEADER=1
+)
+PATTERN='.*<s3_file_name>.*[.]csv[.]gz'
