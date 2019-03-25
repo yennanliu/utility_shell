646b1b8222c3739c4735edb9f1359c1e5801ca55
yennanliu <f339339@gmail.com>
2019-03-25 10:35:15 +0000

    add shell read file by lines demo

---
+++ file_op/bash_read_file_line_by_line.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# https://www.shellhacks.com/bash-read-file-line-by-line-while-read-line-loop/
+
+
+#################################################################
+# DEMO SCRIPT READ FILE LINE BY LINE 
+#################################################################
+
+
+FILE=$1
+while read LINE; do
+     echo "$LINE"
+done < $FILE
