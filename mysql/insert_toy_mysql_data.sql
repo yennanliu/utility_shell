cb2a7ac37731a96f8c93e047221da5ea785843c0
Travis CI <f339339@gmail.com>
2019-07-21 23:54:00 +0800

    add sql insert toy data into mysql

---
+++ mysql/insert_toy_mysql_data.sql
@@ -0,0 +1,63 @@
+# table 1  : exchange_rates
+
+drop table if exists exchange_rates;
+create table exchange_rates(
+ts timestamp ,
+from_currency varchar(3),
+to_currency varchar(3),
+rate numeric
+);
+
+truncate table exchange_rates;
+insert into exchange_rates
+values
+('2018-04-01 00:00:00', 'USD', 'GBP', '0.71'),
+('2018-04-01 00:00:05', 'USD', 'GBP', '0.82'),
+('2018-04-01 00:01:00', 'USD', 'GBP', '0.92'),
+('2018-04-01 01:02:00', 'USD', 'GBP', '0.62'),
+
+('2018-04-01 02:00:00', 'USD', 'GBP', '0.71'),
+('2018-04-01 03:00:05', 'USD', 'GBP', '0.82'),
+('2018-04-01 04:01:00', 'USD', 'GBP', '0.92'),
+('2018-04-01 04:22:00', 'USD', 'GBP', '0.62'),
+
+('2018-04-01 00:00:00', 'EUR', 'GBP', '1.71'),
+('2018-04-01 01:00:05', 'EUR', 'GBP', '1.82'),
+('2018-04-01 01:01:00', 'EUR', 'GBP', '1.92'),
+('2018-04-01 01:02:00', 'EUR', 'GBP', '1.62'),
+
+('2018-04-01 02:00:00', 'EUR', 'GBP', '1.71'),
+('2018-04-01 03:00:05', 'EUR', 'GBP', '1.82'),
+('2018-04-01 04:01:00', 'EUR', 'GBP', '1.92'),
+('2018-04-01 05:22:00', 'EUR', 'GBP', '1.62'),
+
+('2018-04-01 05:22:00', 'EUR', 'HUF', '0.062')
+;
+
+# table 2 :  transactions
+drop table if exists transactions;
+create table transactions (
+ts timestamp,
+user_id int,
+currency varchar(3),
+amount numeric
+);
+
+truncate table transactions;
+insert into transactions
+values
+('2018-04-01 00:00:00', 1, 'EUR', 2.45),
+('2018-04-01 01:00:00', 1, 'EUR', 8.45),
+('2018-04-01 01:30:00', 1, 'USD', 3.5),
+('2018-04-01 20:00:00', 1, 'EUR', 2.45),
+
+('2018-04-01 00:30:00', 2, 'USD', 2.45),
+('2018-04-01 01:20:00', 2, 'USD', 0.45),
+('2018-04-01 01:40:00', 2, 'USD', 33.5),
+('2018-04-01 18:00:00', 2, 'EUR', 12.45),
+
+('2018-04-01 18:01:00', 3, 'GBP', 2),
+
+('2018-04-01 00:01:00', 4, 'USD', 2),
+('2018-04-01 00:01:00', 4, 'GBP', 2)
+;
