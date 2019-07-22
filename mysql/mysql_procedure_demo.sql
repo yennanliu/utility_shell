e1be29337e0a013f28e9b3885b4c0d6f84f436ab
Travis CI <f339339@gmail.com>
2019-07-22 09:24:36 +0800

    rename mysql for-loop and procedure_demo sql

--- mysql/mysql_procedure_demo.sql
+++ mysql/mysql_procedure_demo.sql
@@ -1,11 +1,12 @@
 -- https://stackoverflow.com/questions/14356694/mysql-function-call
--- mysql> CALL simpleproc(@a);
+-- mysql> CALL count_func(@a);
 -- Query OK, 0 rows affected (0.00 sec)
 
 
 delimiter //
 
-CREATE PROCEDURE count_func3 (OUT param1 INT)
+CREATE PROCEDURE count_func (OUT param1 INT)
 BEGIN
   SELECT COUNT(*) INTO param1 FROM Employee;
+  SELECT param1;
 END//
