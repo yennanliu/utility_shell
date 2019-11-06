f610c8994dfe84426bf552cc6c38dc5c0a62064f
yennanliu <f339339@gmail.com>
2019-11-06 09:44:30 +0800

    update s3 commands

--- s3_ops/s3_command.sh
+++ s3_ops/s3_command.sh
@@ -20,5 +20,11 @@ aws s3 sync . s3://<s3_bucket_name/<s3_file_name> --exclude "*.txt"
 aws s3 cp . s3://<s3_bucket_name/<s3_file_name>  --recursive \
     --exclude "*" --include "*.csv.gz" 
 
+# copy all files from s3 to another s3 
+aws s3  cp --recursive s3://<from_s3_bucket_name/<s3_file_name>  s3://<to_s3_bucket_name/<s3_file_name>
+
 # sync 2 s3 bucket (from -> to)
-aws s3 sync s3://s3://<s3_bucket_name/<from_s3_file_name> s3://s3://<s3_bucket_name/<to_s3_file_name>
+aws s3 sync s3://<s3_bucket_name/<from_s3_file_name> s3://s3://<s3_bucket_name/<to_s3_file_name>
+
+# remove file from s3 
+aws s3 rm s3://<s3_bucket_name/<from_s3_file_name> --recursive --exclude "*" --include "xxx_2013*.csv.gz"
