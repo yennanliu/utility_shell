84f18eaf8c4a841785861af1c12d98c5ec6c86bf
yennanliu <f339339@gmail.com>
2019-11-10 08:43:37 +0800

    fix s3 url partition_s3_file.sh

--- s3_ops/partition_s3_file.sh
+++ s3_ops/partition_s3_file.sh
@@ -2,14 +2,13 @@
 #################################################################
 # HELP SCRIPT PARTITION S3 FILES 
 #################################################################
-
-from_bucket=s3://<from_s3_bucket>/<s3_file>
-to_bucket=s3://<to_s3_bucket>/<s3_partiton_file>
-s3_files=`aws s3 ls s3://<from_s3_bucket>/<s3_file> | cut -d " " -f 4`
+from_bucket=s3://suntory-data/raw_transaciton_data
+to_bucket=s3://suntory-data/raw_transaciton_data_partition2
+s3_files=`aws s3 ls s3://suntory-data/raw_transaciton_data/ | cut -d " " -f 4`
 
 for s3_file in $s3_files;
     do 
        to_file=$(echo $s3_file | cut -d " "  -f 4 | cut -d "_" -f 2 | cut -d "." -f 1)
-       echo "copy" $from_bucket/$s3_file "to" $to_bucket/$to_file/$s3_file 
-       aws s3 cp $from_bucket/$s3_file $to_bucket/$to_file/$s3_file
+       echo "copy" $from_bucket/$s3_file "to" $to_bucket/ym=$to_file/$s3_file 
+       aws s3 cp $from_bucket/$s3_file $to_bucket/ym=$to_file/$s3_file
     done 
