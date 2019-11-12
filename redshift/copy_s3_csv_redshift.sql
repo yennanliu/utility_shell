53df145a9a60606b03b268cfff2a3cb453e31a65
yennanliu <f339339@gmail.com>
2019-11-12 12:42:12 +0800

    update command

--- redshift/copy_s3_csv_redshift.sql
+++ redshift/copy_s3_csv_redshift.sql
@@ -1,4 +1,11 @@
+-- simple copy 
 copy  public.movie_ratings 
 from 's3://<s3_bucket>/<s3_file>' 
 iam_role 'arn:aws:iam::xxxxx:role/xxxx'
 IGNOREHEADER 1   delimiter ',';
+
+-- escape csv double quote
+copy  public.transaction_10_vm 
+from 's3://<s3_bucket>/<s3_file>' 
+iam_role 'arn:aws:iam::xxxxx:role/xxxx'
+ IGNOREHEADER 1   delimiter ',' CSV QUOTE '\"' ;
