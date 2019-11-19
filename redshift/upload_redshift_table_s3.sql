37288b8426603d3c7b50cb33d70e002a55564b3a
yennanliu <f339339@gmail.com>
2019-11-19 11:39:35 +0800

    fix command layout

--- redshift/upload_redshift_table_s3.sql
+++ redshift/upload_redshift_table_s3.sql
@@ -1,7 +1,7 @@
 -- https://docs.aws.amazon.com/redshift/latest/dg/t_Unloading_tables.html
 -- https://stackoverflow.com/questions/24681214/unloading-from-redshift-to-s3-with-headers
 
-# 1) select all 
+--  1) select all 
 UNLOAD ('SELECT * FROM <db_name>.<table_name>;')
 TO 's3://<s3_bucket>/<s3_file>/<s3_file_name>.csv' CREDENTIALS 'aws_access_key_id=<aws_access_key_id>;aws_secret_access_key=<aws_secret_access_key>' 
 ALLOWOVERWRITE
@@ -9,7 +9,7 @@ delimiter ','
 HEADER 
 PARALLEL OFF ;
 
-# 2) select limit count  
+-- 2) select limit count  
 UNLOAD ('SELECT * FROM (SELECT * FROM <db_name>.<table_name>) LIMIT 10 ;')
 TO 's3://<s3_bucket>/<s3_file>/<s3_file_name>.csv' CREDENTIALS 'aws_access_key_id=<aws_access_key_id>;aws_secret_access_key=<aws_secret_access_key>' 
 ALLOWOVERWRITE
