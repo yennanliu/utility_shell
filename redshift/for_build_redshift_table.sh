fb42c0e9a75e90767b0e53e979c07dbe02bb0e3f
yennanliu <f339339@gmail.com>
2019-11-29 14:26:11 +0800

    add multiline_sql_for_loop demo replace str in sql with var

--- redshift/for_build_redshift_table.sh
+++ redshift/for_build_redshift_table.sh
@@ -4,12 +4,34 @@
 ####################################################################
 
 base_for_loop() {
-base_sql="select * from table_"
-for i in 1 2 3 4 5
-    do
-        #echo "select * from $i"
-        echo "${base_sql}"$i 
-    done 
+    base_sql="select * from table_"
+    for i in 1 2 3 4 5
+        do
+            #echo "select * from $i"
+            echo "${base_sql}"$i 
+        done 
 }
 
-base_for_loop
+multiline_sql_for_loop() {
+    base_sql="""
+        WITH filtered_vm AS
+          (SELECT sales_date,
+                  branch_number,
+                  customer_number,
+                  equipment_code,
+                  count(DISTINCT product_code)::numeric/max(column_no)::numeric AS item_ratio
+           FROM table
+           GROUP BY 1,
+                    2,
+                    3,
+                    4
+        HAVING item_ratio > 0.5)"""
+    for i in 1 2 3 4 5
+        do
+            SQL=$(echo $base_sql | sed -e "s/\table/table_$i/g")
+            echo $SQL
+        done 
+}
+
+#base_for_loop
+multiline_sql_for_loop
