d81549095ea50b19e6983769050ea0f2fd9a56db
yennanliu <f339339@gmail.com>
2019-11-29 12:00:02 +0800

    make command as bash func, fix code layout

--- redshift/for_build_redshift_table.sh
+++ redshift/for_build_redshift_table.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
-base_sql="select * from"
+#!/bin/sh
+####################################################################
+# SCRIPT FOR LOOP BUILD TABLES IN REDSHIFT WITH PARAM 
+####################################################################
+
+base_for_loop() {
+base_sql="select * from table_"
 for i in 1 2 3 4 5
-do
-	#echo "select * from $i"
-	echo "${base_sql}" $i 
-done
+    do
+        #echo "select * from $i"
+        echo "${base_sql}"$i 
+    done 
+}
+
+base_for_loop
