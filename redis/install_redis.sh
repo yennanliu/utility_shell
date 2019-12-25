c60cca262ab58deaa187adb8ae297915d7df0c93
yennanliu <f339339@gmail.com>
2019-12-25 14:47:48 +0800

    add more remaining steps for install_redis.sh

--- redis/install_redis.sh
+++ redis/install_redis.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 #https://redis.io/topics/quickstart
 
+# STEP 1) download redis 
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
+make test
+
+# STEP 2) copy files
+cd ~ && redis-stable
+sudo cp src/redis-server /usr/local/bin/
+sudo cp src/redis-cli /usr/local/bin/
