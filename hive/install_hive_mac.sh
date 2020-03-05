d3f1fe47440ea60d21b66ab45d3d4c3516b5acfe
yennanliu <f339339@gmail.com>
2020-03-05 12:34:37 +0800

    add hive scripts

---
+++ hive/install_hive_mac.sh
@@ -0,0 +1,3 @@
+# https://qiita.com/giwa/items/dabf0bb21ae242532423
+# https://luckymrwang.github.io/2018/03/14/Install-hive-on-Mac-with-Homebrew/
+# https://www.tutorialspoint.com/hive/hive_installation.htm
