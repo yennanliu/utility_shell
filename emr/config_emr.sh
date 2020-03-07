cfc9ce9934cb240a9dd91b96f7678ad4aeb5f226
yennanliu <f339339@gmail.com>
2020-03-07 09:49:29 +0800

    add install sbt command (config_emr.sh)

--- emr/config_emr.sh
+++ emr/config_emr.sh
@@ -1,3 +1,11 @@
 
 # install git
 sudo yum install git
+
+# install sbt 
+# https://medium.com/@tedherman/compile-scala-on-emr-cb77610559f0
+wget https://github.com/sbt/sbt/releases/download/v0.13.15/sbt-0.13.15.tgz
+tar xf sbt-0.13.15.tgz
+sudo mv sbt /opt
+export PATH=$PATH:/opt/sbt/bin
+sbt
