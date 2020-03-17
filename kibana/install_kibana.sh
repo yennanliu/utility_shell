bf087479ca2d9da30be2deff07252133646f06a2
yennanliu <f339339@gmail.com>
2020-03-17 09:20:32 +0800

    add /kibana scripts

---
+++ kibana/install_kibana.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# https://www.elastic.co/guide/en/kibana/current/brew.html
+
+#################################################################
+# SCRIPT HELP INSTALL kibana  
+#################################################################
+
+brew tap elastic/tap
+brew install elastic/tap/kibana-full
+
+# config file 
+
+# 1) home
+# /usr/local/var/homebrew/linked/kibana-full
+
+# 2) bin 
+# /usr/local/var/homebrew/linked/kibana-full/bin
+
+# 3) config
+# /usr/local/etc/kibana
+
+# 4) data 
+# /usr/local/var/lib/kibana
+
+# 5) log
+# /usr/local/var/log/kibana
+
+# 6) plugin
+# /usr/local/var/homebrew/linked/kibana-full/plugins
+
