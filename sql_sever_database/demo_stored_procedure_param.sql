0a6cd2b0abbc7f95d298426b25e221899bec33ac
yennanliu <f339339@gmail.com>
2020-05-27 16:34:56 +0800

    add multiple param example (demo_stored_procedure_param.sql

--- sql_sever_database/demo_stored_procedure_param.sql
+++ sql_sever_database/demo_stored_procedure_param.sql
@@ -1,3 +1,16 @@
+-- create
 CREATE PROCEDURE yen_test_procedure_param @ID int
 AS
 SELECT @ID; 
+
+-- execute 
+EXEC yen_test_procedure_param 100;  
+
+
+-- create
+CREATE PROCEDURE yen_test_procedure_param2 @ID int , @ID2 int
+AS
+SELECT @ID, @ID2; 
+
+-- execute 
+EXEC yen_test_procedure_param2 100, 9999;  
