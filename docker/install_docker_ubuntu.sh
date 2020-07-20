c2329fd699de19f7792890499b2d4e3d17fd2e05
yennanliu <f339339@gmail.com>
2020-07-20 13:56:44 +0800

    add ref, trouble-shoot command (docker/install_docker_ubuntu.sh

--- docker/install_docker_ubuntu.sh
+++ docker/install_docker_ubuntu.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 # step by step install docker on ubuntu 18-04
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+
+# install docker-compose
+sudo apt install docker-compose
+
+# add docker-compose to docker user group
+# https://oranwind.org/-solution-qi-dong-docker-compose-fa-sheng-error-couldnt-connect-to-docker-daemon-at-httpdockerlocalunixsocket-is-it-running-cuo-wu/ 
+sudo gpasswd -a ${USER} docker
+sudo su
+su elkuser
+
+# run the docker-compose up
+docker-compose up
