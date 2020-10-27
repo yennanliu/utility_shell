bd5eeaa23e8cc0cc5f3b01aeb0ff19c7778a2306
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2020-10-28 07:40:25 +0800

    add yarn commands

---
+++ yarn/yarn_command.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#################################################################
+# USEFUL YARN CLI COMMAND  
+#################################################################
+
+# 1) kill the running job 
+# https://stackoverflow.com/questions/29565716/spark-kill-running-application
+yarn application -kill <application_id>
