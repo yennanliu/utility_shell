e15aec84577551282d42a2b6bf26d57b3b2572dc
yennj12 <f339339@gmail.com>
2020-10-28 13:25:49 +0800

    Update export_var_func.sh

--- export_var_func.sh
+++ export_var_func.sh
@@ -1,5 +1,4 @@
 set_env() {
-
     # assert env
     if [ -z $SPARK_HOME ]; then
         echo "Need SPARK_HOME "
@@ -9,15 +8,13 @@ set_env() {
     export PYTHONPATH=<PYTHONPATH>
     export AIRFLOW_CONFIG_PATH=<AIRFLOW_CONFIG_PATH>
     export JAVA_HOME=<JAVA_HOME>
-
+    
     echo "PYTHONPATH: $PYTHONPATH"
-    echo "LUIGI_CONFIG_PATH: $LUIGI_CONFIG_PATH"
+    echo "AIRFLOW_CONFIG_PATH: $AIRFLOW_CONFIG_PATH"
     echo "JAVA_HOME: $JAVA_HOME"
 }
 
-
 test_run() {
-
     set_env
     echo 'test_run ...'
     for i in 1 2 3 
