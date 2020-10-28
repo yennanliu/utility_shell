5e596d82857fb883d385c67659d8bd7ecede9abd
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2020-10-28 13:24:45 +0800

    add export_var_func.sh

---
+++ export_var_func.sh
@@ -0,0 +1,27 @@
+set_env() {
+
+    # assert env
+    if [ -z $SPARK_HOME ]; then
+        echo "Need SPARK_HOME "
+        exit 1
+    fi
+
+    export PYTHONPATH=<PYTHONPATH>
+    export AIRFLOW_CONFIG_PATH=<AIRFLOW_CONFIG_PATH>
+    export JAVA_HOME=<JAVA_HOME>
+
+    echo "PYTHONPATH: $PYTHONPATH"
+    echo "LUIGI_CONFIG_PATH: $LUIGI_CONFIG_PATH"
+    echo "JAVA_HOME: $JAVA_HOME"
+}
+
+
+test_run() {
+
+    set_env
+    echo 'test_run ...'
+    for i in 1 2 3 
+    do 
+        echo $i
+    done
+}
