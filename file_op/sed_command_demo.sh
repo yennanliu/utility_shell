a6dbb20e6b528ce0cd00c4c4fdfe7c9ec717ecb2
yennj12 <f339339@gmail.com>
2020-11-12 12:16:13 +0800

    Update sed_command_demo.sh

--- file_op/sed_command_demo.sh
+++ file_op/sed_command_demo.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-
 # http://man.linuxde.net/sed
-
-
-
 # 1) 
 echo '------------ DEMO 1 ------------ '
 echo ' #### print df before op ####'
@@ -21,28 +17,14 @@ sed '/car/p'  df_test2.csv
 echo '------------ DEMO 3 ------------ '
 sed 's:car:CAR:g'  df_test2.csv # as same as 's/car/car_xxx/g', when print, replace all "car" with "car_xxx" in df_test2.csv  
 
-
 # 4)
 echo '------------ DEMO 4 ------------ '
 sed '/^$/d' df_test2.csv # delete blank 
-
 
 # 5) 
 echo '------------ DEMO 5 ------------ '
 sed '$d'  df_test2.csv # delete final line 
 
-
 # 6) 
 echo '------------ DEMO 6 ------------ '
 sed 's/^192.168.0.1/&localhost/'  df_test2.csv # replace all 192.168.0.1xxxx -> 192.168.0.1localhost
-
-
-
-
-
-
-
-
-
-
-
