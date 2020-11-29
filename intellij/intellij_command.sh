c8c40f334d6ae53d8bcfc161bf6a2fb2ab496e22
yennj12 <f339339@gmail.com>
2020-11-29 14:00:59 +0800

    Update intellij_command.sh

--- intellij/intellij_command.sh
+++ intellij/intellij_command.sh
@@ -8,3 +8,9 @@ cmd + b
 
 # find where code from originally
 shift + shift 
+
+# Start a new project (with JDK8, sbt, scala for example..)
+# step 1) open inellj with any project
+# step 2) (go to upper menu)
+# step 3) File -> New -> Project from existing source -> choose the project u wanna open -> click "import project from external model" and clck "sbt" -> then click "java8 JDK"
+# step 4) done. your project shoule be opened and built with intellj now.
