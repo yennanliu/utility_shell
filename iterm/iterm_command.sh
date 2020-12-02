f2354bdb8af02723fc2329d500e91c84e355b12f
yennanliu <f339339@gmail.com>
2020-12-02 18:49:33 +0800

    update iterm/iterm_command.sh

--- iterm/iterm_command.sh
+++ iterm/iterm_command.sh
@@ -2,3 +2,11 @@
 
 # 1) change iterm default font size
 # https://superuser.com/questions/967066/is-there-any-way-to-set-the-default-font-to-16pts-in-iterm2
+
+# 2) download layout
+# https://medium.com/ayuth/iterm2-zsh-oh-my-zsh-the-most-power-full-of-terminal-on-macos-bdb2823fb04c
+
+# 3) same commands in multiples windows
+# https://jacoobwang.github.io/2018/06/28/Iterm2%E5%A4%9A%E7%AA%97%E5%8F%A3%E5%90%8C%E6%97%B6%E8%BE%93%E5%85%A5%E5%91%BD%E4%BB%A4/#:~:text=%E5%85%B6%E5%AE%9E%EF%BC%8CiTerm2%20%E6%94%AF%E6%8C%81%E5%90%91%E5%A4%9A,%E2%87%A7(shift)%20%2B%20i%E3%80%82
+# https://rickhw.github.io/2017/09/23/OSX/iTerm2/
+# https://myapollo.com.tw/zh-tw/iterm2-shortcut-keys/
