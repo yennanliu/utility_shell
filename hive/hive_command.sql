d68610083bd35056ec6d35c9b554395d7dedaffb
yennj12 <f339339@gmail.com>
2020-12-08 10:46:41 +0800

    Create hive_command.sql

---
+++ hive/hive_command.sql
@@ -0,0 +1,25 @@
+# useful sql command working with Hive
+
+1) check data strusture (e.g. partition ..)
+desc <table_name>; 
+
+2) use DB
+use <DB>
+
+3) show DB
+show databases;
+
+4) show tables;
+show tables;
+
+5) create table
+CREATE table test_table (a int, b int);
+
+6) create exteranl table
+CREATE external table test_change (a int, b int);
+
+6)' alter table
+ALTER TABLE test_change REPLACE COLUMNS ( a int, b String);
+
+7) drop table
+drop table test_change
