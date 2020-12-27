6e8a4abff4a13909b2f3f06b1e323ce007d53d01
yennj12 <f339339@gmail.com>
2020-12-27 14:58:13 +0800

    Update intellij_command.sh

--- intellij/intellij_command.sh
+++ intellij/intellij_command.sh
@@ -1,16 +1,19 @@
 # FOR MAC OSX
 
-# OFFICIAL SHORTCUT FOR MAC
+# 1) OFFICIAL SHORTCUT FOR MAC
 # https://www.jetbrains.com/help/rider/Reference_Keymap_Rider_OSX.html#top
 
-# find code used in project 
+# 2) find code used in project 
 cmd + b
 
-# find where code from originally
+# 3) find where code from originally
 shift + shift 
 
-# Start a new project (with JDK8, sbt, scala for example..)
+# 4) Start a new project (with JDK8, sbt, scala for example..)
 # step 1) open inellj with any project
 # step 2) (go to upper menu)
 # step 3) File -> New -> Project from existing source -> choose the project u wanna open -> click "import project from external model" and clck "sbt" -> then click "java8 JDK"
 # step 4) done. your project shoule be opened and built with intellj now.
+
+# 5) Change font size
+# preferences -> Editor -> Font
