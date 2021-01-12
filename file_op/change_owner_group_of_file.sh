09950e81b5401a7eef772ed15c46f2550f3fa8bf
yennj12 <f339339@gmail.com>
2021-01-12 08:04:31 +0800

    Create change_owner_group_of_file.sh

---
+++ file_op/change_owner_group_of_file.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# https://linuxize.com/post/linux-chown-command/
+# https://www.youtube.com/watch?v=i2sDhe3jVu0&list=PLmOn9nNkQxJEs3ixNOLlilPnNarGtywL9&index=18
+# chown USER:GROUP FILE
+
+# example :
+# sudo chown dev_user:dev_user /opt/dev/file
