63bc62c83ab96448a8ab81816a44e215216cb437
yennj12 <f339339@gmail.com>
2021-01-12 08:05:38 +0800

    Update hadoop_command.sh

--- hadoop/hadoop_command.sh
+++ hadoop/hadoop_command.sh
@@ -116,3 +116,9 @@ for y in {2018..2021}
     done
  done
  
+# 15) Change the Owner and Group of a File 
+# https://linuxize.com/post/linux-chown-command/
+# https://www.youtube.com/watch?v=i2sDhe3jVu0&list=PLmOn9nNkQxJEs3ixNOLlilPnNarGtywL9&index=18
+# chown USER:GROUP FILE
+# example :
+# sudo chown dev_user:dev_user /opt/dev/file
