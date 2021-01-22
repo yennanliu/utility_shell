d180ef93413fe4a25ec6823fc2517bd423e29377
yennj12 <f339339@gmail.com>
2021-01-22 18:10:07 +0800

    Create show_file.sh

---
+++ file_op/show_file.sh
@@ -0,0 +1,3 @@
+# 0) show all hidden files
+# https://www.tecmint.com/hide-files-and-directories-in-linux/#:~:text=To%20view%20hidden%20files%2C%20run,al%20flag%20for%20long%20listing.&text=From%20a%20GUI%20file%20manager,view%20hidden%20files%20or%20directories.
+ls -al <path>
