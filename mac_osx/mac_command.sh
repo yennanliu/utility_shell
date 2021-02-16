c13a90dad99c1fe58879914f86500ee64d48ca27
yennj12 <f339339@gmail.com>
2021-02-16 10:14:40 +0800

    Create mac_command.sh

---
+++ mac_osx/mac_command.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# 1) show hidden path
+# https://www.ionos.com/digitalguide/server/configuration/showing-hidden-files-on-a-mac/
+# press `cmd + shift + [.] ` in order to show "hidden" path (e.g. /tmp, /opt...)
