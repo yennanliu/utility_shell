9e3e76f07152fcb73b314610538e4e8347db5941
yennj12 <f339339@gmail.com>
2021-03-09 18:25:56 +0800

    Update hive_command.sql

--- hive/hive_command.sql
+++ hive/hive_command.sql
@@ -8,14 +8,20 @@ desc <table_name>;
 # 1)' show partition
 show partitions <table_name> ;
 
+# 1)'' create db
+CREATE DATABASE  test_db;
+
 # 2) use DB
-use <DB>
+use test_db;
 
 # 3) show DB
 show databases;
 
 # 4) show tables;
 show tables;
+
+# 4)' show table details
+DESCRIBE FORMATTED test_table PARTITION (my_column='my_value');
 
 # 5) create table
 CREATE table test_table (a int, b int);
@@ -28,3 +34,14 @@ ALTER TABLE test_change REPLACE COLUMNS ( a int, b String);
 
 # 7) drop table
 drop table test_change
+
+# 8) load external jar (deserialize json for example)
+SET LIB_DIR = /usr/local/common/lib;
+ADD JAR ${hiveconf:LIB_DIR}/json-serde-1.3.9-jar-with-dependencies.jar;
+
+# 9) alter table
+ALTER TABLE test_table SET SERDEPROPERTIES ('ignore.malformed.json' = 'true');
+
+# 10) load HDFS data into table (dev)
+# load hdfs raw data into hive table
+load data inpath '<hdfs_data_path>' into table test_table; --partition(uid=1557);
