1e7265c3ffbbb1934c1a33bcf0011acad8b4f75c
yennj12 <f339339@gmail.com>
2021-03-29 16:09:06 +0800

    Update install_docker_centos.sh

--- docker/install_docker_centos.sh
+++ docker/install_docker_centos.sh
@@ -1,1 +1,20 @@
 # ref : https://docs.docker.com/engine/install/centos/
+# login as root user
+
+# uninstall old versions
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+# install Docker Engine
+sudo yum install docker-ce docker-ce-cli containerd.io --skip-broken
