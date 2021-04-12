4b11380250099e5b973db11e23bff736723aafb2
yennj12 <f339339@gmail.com>
2021-04-12 16:32:26 +0800

    Update file_op_command.sh

--- file_op/file_op_command.sh
+++ file_op/file_op_command.sh
@@ -4,3 +4,8 @@
 # rm -v -- "#file"
 rm -v --  mapred.reduce.tasks=2
 rm -v --  -Dmapred.job.reduce.memory.mb=8192
+
+# 2) grep a file, but show several surrounding lines
+# https://stackoverflow.com/questions/9081/grep-a-file-but-show-several-surrounding-lines
+# grep -B 3 -A 2 foo README.txt
+cat my_job.log | grep Failed -B 3 -A 2
