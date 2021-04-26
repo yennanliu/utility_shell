8db40f351d12fdd95563cafe3fb692512c788a1e
yennj12 <f339339@gmail.com>
2021-04-26 15:22:04 +0800

    Update centos_install.sh

--- centos/centos_install.sh
+++ centos/centos_install.sh
@@ -1,5 +1,12 @@
+# 1)
 # centos install ansible plaubook
 # https://www.snel.com/support/how-to-install-ansible-on-centos-7/
-
 sudo yum -y install epel-repo
 sudo yum -y install ansible
+
+
+# 2)
+# cent os install apt-get
+# https://www.getpagespeed.com/server-setup/apt-centos
+curl https://raw.githubusercontent.com/dvershinin/apt-get-centos/master/apt-get.sh -o /usr/local/bin/apt-get
+chmod 0755 /usr/local/bin/apt-get
