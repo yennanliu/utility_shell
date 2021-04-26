80f06e24bffe687727e2ad04dfec2a1c4b04b329
yennj12 <f339339@gmail.com>
2021-04-26 15:56:51 +0800

    Update install_docker.sh

--- centos/install_docker.sh
+++ centos/install_docker.sh
@@ -1,10 +1,11 @@
 # ref
+# 0) https://blog.gtwang.org/linux/centos-linux-7-install-docker-tutorial/
 # 1) sudo yum install docker-ce-17.03.1.ce-1.el7.centos docker-ce-cli-17.03.1.ce-1.el7.centos containerd.io
 # 2) https://linoxide.com/how-to-install-docker-on-centos/
 # 3) https://qiita.com/smallpalace/items/57bf018909644e7c3e70
 
 # install docker command  (dev)
-sudo yum install docker-ce-3:19.03.4-3.el7.x86_64  docker-ce-3:19.03.4-3.el7.x86_64  containerd.io --nobest
+sudo yum install docker-ce-3:19.03.4-3.el7.x86_64  docker-ce-3:19.03.4-3.el7.x86_64  containerd.io
 
 
 # extra : clean docker, meta, relative files:
