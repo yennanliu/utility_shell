520c9a5fef7810d1575580828d15fd76e239e466
yennj12 <f339339@gmail.com>
2021-05-31 10:38:55 +0800

    Update centos_install.sh

--- centos/centos_install.sh
+++ centos/centos_install.sh
@@ -10,3 +10,8 @@ sudo yum -y install ansible
 # https://www.getpagespeed.com/server-setup/apt-centos
 curl https://raw.githubusercontent.com/dvershinin/apt-get-centos/master/apt-get.sh -o /usr/local/bin/apt-get
 chmod 0755 /usr/local/bin/apt-get
+
+# 3) install javac
+# https://stackoverflow.com/questions/5407703/javac-command-not-found
+# as root
+yum install java-devel
