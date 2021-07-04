ca2c3c7fd526ad42eb31c85d5afc97d54a4d8a33
yennanliu <yennanliu@yennanliudeMacBook-Air.local>
2021-07-04 15:24:46 +0800

    add mac_osx_m1

---
+++ mac_osx_m1/install_java.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# 1) install JAVA 8 on mac osx M1
+# ref 1 : https://www.jianshu.com/p/c85349d6310e
+# ref 2 : https://alexmanrique.com/blog/development/2021/01/05/first-steps-using-java-in-macbook-air-m1.html
+
+# downlaod java 1.8 JDK :
+# https://www.azul.com/downloads/zulu-community/?os=macos&architecture=arm-64-bit&package=jdk
