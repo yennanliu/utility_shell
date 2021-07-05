f045119d06812c4777d8248593864884c86a04cb
yennj12 <f339339@gmail.com>
2021-07-05 15:07:58 +0800

    Create paste_with_format.sh

---
+++ centos/paste_with_format.sh
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/2514445/turning-off-auto-indent-when-pasting-text-into-vim
+
+vi <file_name>
+# in the edit mood
+:set paste
+# then press i, should see the insert <paste> mood
+i
+# paste code with format
