363028c4aa2e346346e32c4b391c8b3683833ff2
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-07-12 11:21:16 +0800

    update

--- redis/install_redis_mac.sh
+++ redis/install_redis_mac.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # https://gist.github.com/tomysmile/1b8a321e7c58499ef9f9441b2faa0aa8
 
 brew update
@@ -5,8 +7,8 @@ brew install redis
 
 brew services start redis
 
-# test
+# test (local)
 redis-cli ping
-
-# to the redis shell
+ 
+# to the redis shell (local)
 redis-cli
---
+++ redis/redis_command.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# 1) connect to remote redis
+# https://www.1ju.org/redis/redis-quick-guide
+redis-cli -h host -p port -a password
