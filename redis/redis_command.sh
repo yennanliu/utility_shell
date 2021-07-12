9517ae8765566f50f40c12fbf6b7acca475bcd53
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-07-12 13:52:23 +0800

    update

--- redis/redis_command.sh
+++ redis/redis_command.sh
@@ -3,3 +3,14 @@
 # 1) connect to remote redis
 # https://www.1ju.org/redis/redis-quick-guide
 redis-cli -h host -p port -a password
+# or below for solving "Redisへのコマンド実行時に発生する”(error) MOVED”エラーを解決する方法" issue
+# https://dev.classmethod.jp/articles/how-to-handle-redis-cli-moved-error/
+redis-cli -c -h host -p port -a password
+
+# 2) get all keys
+# https://redis.io/commands/KEYS
+keys * 
+
+# 3) get keys with pattern
+# pattern : *name*
+keys *names*
