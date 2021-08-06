1404a4208776e318c8c8abbc8a89c7dd40f48906
yennj12 <f339339@gmail.com>
2021-08-06 10:00:42 +0800

    Update mac_command.sh

--- mac_osx/mac_command.sh
+++ mac_osx/mac_command.sh
@@ -3,3 +3,11 @@
 # 1) show hidden path
 # https://www.ionos.com/digitalguide/server/configuration/showing-hidden-files-on-a-mac/
 # press `cmd + shift + [.] ` in order to show "hidden" path (e.g. /tmp, /opt...)
+
+# 2) show macbook boost time (launch time)
+# METHOD 1)
+# # https://kknews.cc/news/l9yepb9.html
+# application -> Console.app -> system.log
+# METHOD 2)
+# https://zhidao.baidu.com/question/463469527.html
+# terminal -> last | grep reboot
