ea1e301eac32878867dcab2b18e19ba1dee20968
yennj12 <f339339@gmail.com>
2021-09-16 18:06:24 +0800

    Create install_sbt.sh

---
+++ centos/install_sbt.sh
@@ -0,0 +1,6 @@
+# step 1) download
+# https://www.scala-sbt.org/download.html
+wget https://github.com/sbt/sbt/releases/download/v0.13.18/sbt-0.13.18.tgz
+
+# step 2) unzip (should unzip to sbt file)
+tar zxvf sbt-0.13.18.tgz
