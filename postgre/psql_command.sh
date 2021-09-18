294fca7a4b2a61b9d4870f177b52a2f528fe7a34
yennj12 <f339339@gmail.com>
2021-09-18 14:36:42 +0800

    Update psql_command.sh

--- postgre/psql_command.sh
+++ postgre/psql_command.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-
 #################################################################
 # USEFUL COMMAND FOR ACCESSING POSGRE DB VIA PSQL 
 #################################################################
 
-
 # https://dba.stackexchange.com/questions/1285/how-do-i-list-all-databases-and-tables-using-psql
 # https://stackoverflow.com/questions/10335561/use-database-name-command-in-postgresql
-
 
 <<COMMENT1
 yennanliu@yennanliu-MacBook-Pro:~/data_science_repo/de_airflow_docker_dev$ psql
@@ -56,10 +53,9 @@ pgguide=# \q
 yennanliu@yennanliu-MacBook-Pro:~/data_science_repo/de_airflow_docker_dev$ 
 COMMENT1
 
-
-
 # 1) connect to local posgre via psql 
-psql 
+psql
+# or psql -U postgres --password and password : 0000 (ur computer pwd)
 
 # 2) list all database 
 \l 
@@ -72,5 +68,3 @@ psql
 
 # 5) quit psql 
 \q 
-
-
