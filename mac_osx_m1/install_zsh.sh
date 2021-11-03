4c2c873cadd3e4bf50090d6e31d664a597fd9252
yennj12 <f339339@gmail.com>
2021-11-03 15:01:24 +0800

    Create install_zsh.sh

---
+++ mac_osx_m1/install_zsh.sh
@@ -0,0 +1,13 @@
+# https://technofob.com/2020/12/24/the-ultimate-mac-m1-terminal-iterm2-oh-my-zsh-zsh-syntax-highlighting/
+
+# step 1) install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# step 2) Install zsh-autosuggestions
+it clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# step 4) activate
+# vi ~/.zshrc
+# plugins=(git zsh-autosuggestions)
+
+# step 5) customize .zshrc
