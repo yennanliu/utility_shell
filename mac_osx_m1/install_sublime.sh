11c5d77a591d651cba39934593dafc0616129689
yennj12 <f339339@gmail.com>
2021-11-06 15:19:43 +0800

    Create install_sublime.sh

---
+++ mac_osx_m1/install_sublime.sh
@@ -0,0 +1,6 @@
+# https://stackoverflow.com/questions/25152711/subl-command-not-working-command-not-found
+$ ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/.
+$ echo $PATH
+
+# test
+subl .
