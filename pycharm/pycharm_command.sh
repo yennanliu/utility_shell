bd8647e39b2bd211482e46e51cab87b565dffe42
yennanliu <f339339@gmail.com>
2021-11-15 07:54:36 +0800

    update pycharm cmd

---
+++ pycharm/pycharm_command.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# 1) Pycharm use existing virtual env
+# https://blog.csdn.net/seven_zhao/article/details/71636560
+# https://www.jianshu.com/p/cef6f9f51466
+
+# ref : https://github.com/yennanliu/utility_shell/blob/master/doc/pic/pycharm_env1.png
+# File -> new prject -> select ur existing project -> new environment using : conda -> location (select ur virtual env) -> Conda executable : /opt/anaconda3/bin/conda-> done
