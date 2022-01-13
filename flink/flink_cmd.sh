e8e63c2834ee80d996f2fee7ec931ec10f4b9444
yennj12 <f339339@gmail.com>
2022-01-14 07:05:23 +0800

    Update flink_cmd.sh

--- flink/flink_cmd.sh
+++ flink/flink_cmd.sh
@@ -15,3 +15,7 @@
  
 # cancel the job with save point
 ~/flink/bin/flink cancel -s <job_id> 
+
+# generate save point
+# https://github.com/yennanliu/utility_shell/blob/master/flink/save_point.sh
+bash /utility_shell/flink/save_point.sh <JM_hostname> '<FLINK_JOB_NAME>' > "$HOME/savepoint/savepoint_<FLINK_JOB_NAME>_$(date +\%Y\%m\%d\%H\%M\%S)"
