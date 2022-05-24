e6d565e2a962cc68566a9ab56447d51efc4173e4
yennanliu <f339339@gmail.com>
2022-05-24 22:24:30 +0800

    update script

--- mongo/mongo_cmd.sh
+++ mongo/mongo_cmd.sh
@@ -1,6 +1,9 @@
 # install
 # https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/
-brew install mongodb-community@5.0
+brew tap mongodb/brew
+brew update
+
+rew install mongodb-community@5.0
 
 # run
 brew services start mongodb-community@5.0
