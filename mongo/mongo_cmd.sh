f2d28fc57f5752bdd93f58db31adbf10e9427cb9
yennanliu <f339339@gmail.com>
2022-05-24 22:20:57 +0800

    add mongo cmd

---
+++ mongo/mongo_cmd.sh
@@ -0,0 +1,10 @@
+# install
+# https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/
+brew install mongodb-community@5.0
+
+# run
+brew services start mongodb-community@5.0
+
+# stop
+brew services stop mongodb-community@5.0
+
