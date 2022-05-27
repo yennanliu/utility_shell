2c6bfc68a979056c13219a606da5b7336a35eee9
yen.liu <yen.l@binance.com>
2022-05-27 14:38:13 +0800

    add postman

---
+++ postman/postman_cmd.sh
@@ -0,0 +1,6 @@
+# 1) Add new API call to postman
+# step 1 : go to target url
+# step 2 : open "developer tool" and filter "Fetch/XHR"
+# step 3 : right click "Request URL" and "copy"  -> "copy as cURL"
+# step 4 : go to postman, click "import" -> "Raw text" -> paste what we copy on step 3, and "Continue"
+# step 5 : done! we should be able to see and test our API endpoint in postman
