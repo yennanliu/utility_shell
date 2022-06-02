9efd8ace374a5e3676d75e00fdaa5710b37e42f2
yennj12 <f339339@gmail.com>
2022-06-02 09:24:11 +0800

    Update iterm_command.sh

--- iterm/iterm_command.sh
+++ iterm/iterm_command.sh
@@ -10,3 +10,8 @@
 # https://jacoobwang.github.io/2018/06/28/Iterm2%E5%A4%9A%E7%AA%97%E5%8F%A3%E5%90%8C%E6%97%B6%E8%BE%93%E5%85%A5%E5%91%BD%E4%BB%A4/#:~:text=%E5%85%B6%E5%AE%9E%EF%BC%8CiTerm2%20%E6%94%AF%E6%8C%81%E5%90%91%E5%A4%9A,%E2%87%A7(shift)%20%2B%20i%E3%80%82
 # https://rickhw.github.io/2017/09/23/OSX/iTerm2/
 # https://myapollo.com.tw/zh-tw/iterm2-shortcut-keys/
+
+# 4) change default font size
+# https://superuser.com/questions/967066/is-there-any-way-to-set-the-default-font-to-16pts-in-iterm2
+# Open 'Preferences', click on the 'Profiles' tab, further click on the 'Text' sub-tab,
+# steps) Preferences -> Profiles -> Font -> change ur font size!
