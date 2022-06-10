588374881ce71d93dd58f07c77e0dd19bede4a99
yennj12 <f339339@gmail.com>
2022-06-10 10:22:46 +0800

    Create cmd.sh

---
+++ dbeaver/cmd.sh
@@ -0,0 +1,2 @@
+# 1) show DataBase Navigator again
+# step) : open DBeaver -> click magnifier (放大鏡) on the right hand side -> search "DataBase Navigator" -> click it -> DataBase Navigator should show up
