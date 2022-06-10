0cc5ea586e876255902190e5c5e8e87f5cf2d79a
yennj12 <f339339@gmail.com>
2022-06-10 08:52:17 +0800

    Create sublime_cmd.sh

---
+++ sublime/sublime_cmd.sh
@@ -0,0 +1,4 @@
+# 1) change color scheme
+# https://stackoverflow.com/questions/27368674/how-to-change-background-and-text-colors-in-sublime-text-3
+# For How do I change the overall colors (background and font)?
+# For MAC : goto Sublime text -> Preferences -> color scheme
