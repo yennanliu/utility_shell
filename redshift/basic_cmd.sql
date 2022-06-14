043803388ab5b170ccdcf23b1f94bfaef7c8abe1
yen.liu <yen.l@binance.com>
2022-06-14 09:10:58 +0800

    add

--- redshift/basic_cmd.sql
+++ redshift/basic_cmd.sql
@@ -20,3 +20,27 @@ values
 (3, 'Kim', 'UK', 'LDN'),
 (4, 'Ana', 'FR', 'PARIS'),
 (5, 'Jack', 'JP', 'TOKYO');
+
+
+----------------------------------
+-- 1) create table and insert random data
+----------------------------------
+
+
+CREATE TABLE date_dimension (
+    id INTEGER primary key,
+    date VARCHAR,
+    month VARCHAR
+);
+
+INSERT INTOdate_dimension ..
+select ......   
+ from 
+  (
+    SELECT
+      '2020-12-31' :: DATE + seq AS datum,
+      seq
+    FROM (SELECT row_number() over () AS seq FROM stl_scan LIMIT 365 ) 
+    ORDER BY seq
+  ) 
+
