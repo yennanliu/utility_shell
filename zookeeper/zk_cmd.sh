8e395dec5a939aa7d552467b48e381f11059db3b
yennj12 <f339339@gmail.com>
2022-06-19 07:40:48 +0800

    Update zk_cmd.sh

--- zookeeper/zk_cmd.sh
+++ zookeeper/zk_cmd.sh
@@ -1,3 +1,10 @@
 # 1) kill/run zk via brew
 brew services start zookeeper
 brew services stop zookeeper
+
+# 2) ZK CLI example
+# https://youtu.be/eKnWj_rDQO0?t=553
+# step 1) /.zkServer.sh start
+# step 2) /.zkCli.sh
+# step 3) (inside ZK) ls /
+# step 4) (inside ZK) get /zookeeper
