57984090d0389bce2bdb57b624931a3ff1ff8e00
yennj12 <f339339@gmail.com>
2022-06-24 18:42:15 +0800

    Update mac_cmd.sh

--- mac_apple_cpu_m1_pro/mac_cmd.sh
+++ mac_apple_cpu_m1_pro/mac_cmd.sh
@@ -1,3 +1,6 @@
 # 1) kill process use port xxx
 # https://stackoverflow.com/questions/3855127/find-and-kill-process-locking-port-3000-on-mac
-lsof -i tcp:8888 
+lsof -i tcp:8888
+
+# 2) kill all java processes
+# mac -> Launchedpad -> activity monitor -> filter by "java" -> force quit processes
