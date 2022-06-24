3b0d60c8d60faa3ae797f7b33941f2aa0c577bce
yennj12 <f339339@gmail.com>
2022-06-24 17:24:52 +0800

    Update postman_cmd.sh

--- postman/postman_cmd.sh
+++ postman/postman_cmd.sh
@@ -4,3 +4,14 @@
 # step 3 : right click "Request URL" and "copy"  -> "copy as cURL"
 # step 4 : go to postman, click "import" -> "Raw text" -> paste what we copy on step 3, and "Continue"
 # step 5 : done! we should be able to see and test our API endpoint in postman
+
+# 2) Send `Map<String, Object>` with POST request via Postname ?
+#     - step 1) set up url (endpoint), choose POST type
+#     - step 2) Body -> raw -> choose "Json" type
+#     - step 3) add content into body (Map<String, Object>)
+#     - step 4) send request
+#     - <img src ="https://github.com/yennanliu/SpringPlayground/blob/main/DownloadServiceV2/doc/img/post_with_map_1.png">
+#     - <img src ="https://github.com/yennanliu/SpringPlayground/blob/main/DownloadServiceV2/doc/img/post_with_map_2.png">
+#     - Ref
+#         - http://www.codebaoku.com/it-java/it-java-220952.html
+#         - https://github.com/yennanliu/SpringPlayground/tree/main/DownloadServiceV2
