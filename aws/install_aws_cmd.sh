ca3b84480d9935452cefed300ac9623aaf72feca
yennj12 <f339339@gmail.com>
2022-08-03 08:17:40 +0800

    Create install_aws_cmd.sh

---
+++ aws/install_aws_cmd.sh
@@ -0,0 +1,4 @@
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+# (mac)
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
