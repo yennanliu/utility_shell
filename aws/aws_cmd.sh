74bb898bd650085b8a8fbdeea26c4e95714b93e8
yennj12 <f339339@gmail.com>
2022-08-03 08:34:47 +0800

    Update and rename install_aws_cmd.sh to aws_cmd.sh

---
+++ aws/aws_cmd.sh
@@ -0,0 +1,18 @@
+# 1) install AWS S3 CMD (Mac)
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+
+# 2) config it
+# https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html#Using_CreateAccessKey
+# NOTE !!! you can get  AWS Access Key ID, AWS Secret Access Key via IAm page
+aws configure
+# AWS Access Key ID [None]: yyy
+# AWS Secret Access Key [None]: xxx
+# Default region name [None]: ap-northeast-1
+
+# 3) list S3 bucket
+aws s3 ls
+
+# 4) copy local file to S3 bucket
+aws s3 cp user.sql s3://yen-bucket1
--- aws/install_aws_cmd.sh
+++
@@ -1,4 +0,0 @@
-# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-# (mac)
-curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-sudo installer -pkg AWSCLIV2.pkg -target /
