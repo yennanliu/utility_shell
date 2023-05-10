03b0201155d178cf65c6fe7ae0a46be28e4df5b2
yennj12 <f339339@gmail.com>
2023-05-10 10:00:15 +0100

    Create basic.sh

---
+++ basic.sh
@@ -0,0 +1,5 @@
+# Basic useful bash command
+
+# 1) Linux : Search for a Particular word in a List of files under a directory
+# https://stackoverflow.com/questions/9665292/linux-search-for-a-particular-word-in-a-list-of-files-under-a-directory
+grep -rn "key_word" <to_search_directory>
