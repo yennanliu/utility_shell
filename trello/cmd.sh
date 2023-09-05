6fa3ef338e254a574a247a1544d7a73737d1c3b0
yennj12 <f339339@gmail.com>
2023-09-05 07:39:57 +0100

    Create cmd.sh

---
+++ trello/cmd.sh
@@ -0,0 +1,3 @@
+# 1) Dark <--> Light mode
+# https://blog.trello.com/introducing-trello-dark-mode
+# You can toggle between light and dark themes on your Trello boards by clicking the circle in the top right corner of your Trello boards.
