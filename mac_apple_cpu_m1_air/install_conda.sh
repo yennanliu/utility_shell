1239c3b0d0e4c9a1ecbfe7e06e0b571432134d44
yennj12 <f339339@gmail.com>
2023-11-08 15:17:34 +0800

    Update install_conda.sh

--- mac_apple_cpu_m1_air/install_conda.sh
+++ mac_apple_cpu_m1_air/install_conda.sh
@@ -1,5 +1,25 @@
 # https://zhuanlan.zhihu.com/p/349295868
 
+#-----------------------------------
+# V1
+#-----------------------------------
+
+# https://docs.conda.io/projects/miniconda/en/latest/
+
+# install
+mkdir -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+# export env var
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+
+
+#-----------------------------------
+# V2
+#-----------------------------------
 # Step 1) install Conda for Apple silicon (M1) with below link
 # https://zhuanlan.zhihu.com/p/349295868#:~:text=https%3A//github.com/conda-forge/miniforge/%23download
 
