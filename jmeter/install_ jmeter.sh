9a61335ec5d229f9d0a0aed187cb9a7f0f9432ac
yennj12 <f339339@gmail.com>
2023-12-13 09:54:08 +0800

    Update install_ jmeter.sh

--- jmeter/install_ jmeter.sh
+++ jmeter/install_ jmeter.sh
@@ -9,3 +9,8 @@ open /usr/local/bin/jmeter
 # or if already update env var in bash_profile
 # https://www.jianshu.com/p/92b63a629922
 jmeter
+
+# basic op for pressure test
+# https://igouist.github.io/post/2022/10/jmeter/
+# https://lufor129.medium.com/%E6%B8%AC%E5%A5%BD%E6%B8%AC%E6%BB%BF-%E4%B8%80-%E5%A3%93%E5%8A%9B%E6%B8%AC%E8%A9%A6jmeter-5356b5335628
+# https://www.astralweb.com.tw/jmeter-website-stress-testing-tutorial/
