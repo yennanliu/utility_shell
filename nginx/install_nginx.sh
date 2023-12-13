e379ae64e352e7a9b384dc80b75ca2c23656765c
yennj12 <f339339@gmail.com>
2023-12-13 10:58:14 +0800

    Update install_nginx.sh

--- nginx/install_nginx.sh
+++ nginx/install_nginx.sh
@@ -1,3 +1,19 @@
 # Install nginx
 
 brew install nginx
+
+# start nginx service
+brew services start nginx
+
+# stop
+brew services stop nginx
+
+# default port: 8080
+# http://localhost:8080/
+
+
+# default config
+# /usr/local/etc/nginx
+# /usr/local/etc/nginx/nginx.conf 
+
+# example setting
