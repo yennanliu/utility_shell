f144ca2a84f610253686cd7ec3bebc8782ee95cd
yennj12 <f339339@gmail.com>
2023-12-14 09:38:18 +0800

    Update install_ jmeter.sh

--- jmeter/install_ jmeter.sh
+++ jmeter/install_ jmeter.sh
@@ -1,7 +1,19 @@
 # 1) mac install jmeter
-# https://stackoverflow.com/questions/22610316/how-do-i-install-jmeter-on-a-mac
+# https://timegoesby.top/Jmeter-For-Mac/
 
-brew install jmeter
+
+#------------------------------------
+# NOTE !!! DO NOT install with brew
+#------------------------------------
+
+# Step 1) go to the website download binary file
+# https://jmeter.apache.org/download_jmeter.cgi
+
+# Step 2) unzip the file, and put to your workspace (e.g. /Users/yennanliu)
+
+# Step 3) run the app
+cd apache-jmeter-5.6.2
+bash bin/jmeter
 
 # run jmeter app
 open /usr/local/bin/jmeter
@@ -10,7 +22,11 @@ open /usr/local/bin/jmeter
 # https://www.jianshu.com/p/92b63a629922
 jmeter
 
-# basic op for pressure test
+
+
+
+
+# 2) basic op for pressure test
 # https://igouist.github.io/post/2022/10/jmeter/
 # https://lufor129.medium.com/%E6%B8%AC%E5%A5%BD%E6%B8%AC%E6%BB%BF-%E4%B8%80-%E5%A3%93%E5%8A%9B%E6%B8%AC%E8%A9%A6jmeter-5356b5335628
 # https://www.astralweb.com.tw/jmeter-website-stress-testing-tutorial/
