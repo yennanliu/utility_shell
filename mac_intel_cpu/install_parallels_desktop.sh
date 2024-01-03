d471cda801af8b3e1c8cab889db012a109ee8634
yennj12 <f339339@gmail.com>
2024-01-03 09:39:59 +0800

    Create install_parallels_desktop.sh

---
+++ mac_intel_cpu/install_parallels_desktop.sh
@@ -0,0 +1,6 @@
+# Ref
+
+# https://www.digit77.com/macapps/parallels-desktop-business-edition/
+# https://mac.macxf.com/mac/3838.html?id=NjU2MTE%3D
+# https://kwaon.wordpress.com/2022/11/19/parallels-desktop-18/
+# 
