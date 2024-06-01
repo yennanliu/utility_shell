295aca25bdcd63c5643e1ff9b7114e86a0b1f763
yennanliu <f339339@gmail.com>
2024-06-01 15:39:12 +0800

    add

--- mac_apple_cpu_m1_air/install_zsh.sh
+++ mac_apple_cpu_m1_air/install_zsh.sh
@@ -4,7 +4,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # step 2) Install zsh-autosuggestions
-it clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # step 4) activate
 # vi ~/.zshrc
