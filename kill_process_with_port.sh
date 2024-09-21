8dcb8c4c1869fe2f22d6554d8fe62ec430e36f6e
yennanliu <f339339@gmail.com>
2024-09-21 13:48:21 +0800

    add, rename script

---
+++ kill_process_with_port.sh
@@ -0,0 +1,14 @@
+# Specify the port you want to kill processes on
+port=8080
+
+# Find the PIDs using the specific port
+pids=$(lsof -i :$port -t)
+
+# Check if any PIDs were found
+if [ -z "$pids" ]; then
+    echo "No processes found using port $port."
+else
+    # Kill the processes
+    kill -9 $pids
+    echo "Killed processes using port $port with PIDs: $pids"
+fi
