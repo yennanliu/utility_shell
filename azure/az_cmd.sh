3b19a3acbae7f078c18738845c3453bddf183160
yennj12 <f339339@gmail.com>
2025-07-15 10:52:36 +0800

    Update az_cmd.sh

--- azure/az_cmd.sh
+++ azure/az_cmd.sh
@@ -1,3 +1,10 @@
 # 1) remove resource group
 #az group delete --name  database-resources --yes --no-wait
 az group delete --name  <resource_name> --yes --no-wait
+
+
+# 2) get az password
+az ad sp create-for-rbac --name "github-actions-acr" \
+                        --role contributor \
+                        --scopes /subscriptions/<SUBSCRIPTION_ID>/resourceGroups/<RESOURCE_GROUP> \
+                        --sdk-auth
