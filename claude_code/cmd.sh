ee260d3f2ea7404728e74022564326fc39cf044a
yennj12 <f339339@gmail.com>
2025-08-06 06:49:14 +0800

    Create cmd.sh

---
+++ claude_code/cmd.sh
@@ -0,0 +1,30 @@
+# 1. stop/interrupt
+
+esc
+
+# 2. Add memory
+
+#
+
+# 3. revert to prev msg
+
+esc +  esc
+
+# 4. Mention a file
+
+@<file_name>
+
+
+# 5. Summary the current session as a single message so that next one can use 
+
+/compact
+
+# 6. Clear previous msg
+
+/clear
+
+# 7. custom cmd
+
+# create a new file:   `.claude/commands`
+
+# under the  `.claude/commands` create `audit.md`
