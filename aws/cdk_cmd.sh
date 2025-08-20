d0a55f45534165c11a7aeca67dc41ba4f77fd857
yennj12 <f339339@gmail.com>
2025-08-20 14:59:06 +0800

    Update cdk_cmd.sh

--- aws/cdk_cmd.sh
+++ aws/cdk_cmd.sh
@@ -12,4 +12,27 @@ aws configure
 # 4) delete all stack with suffix name (`newopensearchdomaind`)
  for stack in $(aws cloudformation list-stacks --query
   "StackSummaries[?contains(StackName, 'newopensearchdomaind')].StackName" --output text);
-   do aws cloudformation delete-stack --stack-name "$stack"; done
+   do 
+      aws cloudformation delete-stack --stack-name "$stack"; 
+   done
+
+# 5) clear CDK cache
+ rm -rf cdk.out/
+ rm -rf node_modules/.cache/
+ rm -rf ~/.cdk.staging/
+
+ # Clear npm cache (if using npm)
+ npm cache clean --force
+
+ # Or clear yarn cache (if using yarn)
+ yarn cache clean
+
+ # Rebuild and redeploy
+ cdk destroy --force
+ cdk deploy --require-approval never
+
+ If you want to be more targeted, you can also try:
+
+ # Just clear the specific stack cache
+ rm -rf cdk.out/
+ cdk deploy --require-approval never --force
