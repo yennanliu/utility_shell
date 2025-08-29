7fc2d217a12d82281ce35cf40f9ea58d23066d12
yennj12 <f339339@gmail.com>
2025-08-29 10:12:56 +0800

    Create cmd.sh

---
+++ vscode/cmd.sh
@@ -0,0 +1,10 @@
+# 1) Read markdown file
+
+# Built-in VS Code features
+# Ctrl+Shift+V
+
+
+# •	You can toggle a Markdown preview with:
+# •	Ctrl+Shift+V (preview in current tab)
+# •	Ctrl+K V (preview to side)
+# •	This gives you real-time rendering of the .m
