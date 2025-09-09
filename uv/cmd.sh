2fe3e82257d7e2e4ab031434111349c01035e4f7
yennj12 <f339339@gmail.com>
2025-09-09 14:03:55 +0800

    Update cmd.sh

--- uv/cmd.sh
+++ uv/cmd.sh
@@ -8,14 +8,18 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 # 1) create new env
+# go to a project path
 $ uv venv my-env
 $ uv venv my-env --python 3.11
 
-# 2) use (activate) new env
-$ uv venv my-env
+# 2) use (activate) new env (??_
+# $ uv venv my-env
 
 # 4) list envs
 
 # 5) Creating a Project with an Environment (uv init)
 $ uv init my-project
 $ cd my-project
+
+# 6) install dep
+uv pip sync requirements.txt
