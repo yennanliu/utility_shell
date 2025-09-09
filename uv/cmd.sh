085c198999df768cf05e0e19553bac41f4d13d10
yennj12 <f339339@gmail.com>
2025-09-09 13:56:35 +0800

    Create cmd.sh

---
+++ uv/cmd.sh
@@ -0,0 +1,15 @@
+# install
+# https://docs.astral.sh/uv/#highlights
+
+# 1) create new env
+$ uv venv my-env
+$ uv venv my-env --python 3.11
+
+# 2) use (activate) new env
+$ uv venv my-env
+
+# 4) list envs
+
+# 5) Creating a Project with an Environment (uv init)
+$ uv init my-project
+$ cd my-project
