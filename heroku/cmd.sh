462b436a80440a58e282d58d8c9c939b7e3dabe9
yennj12 <f339339@gmail.com>
2025-09-18 11:19:13 +0800

    Update cmd.sh

--- heroku/cmd.sh
+++ heroku/cmd.sh
@@ -6,13 +6,11 @@
 # Login to your Heroku account:
 # heroku login
 
-
 # ⸻
 
 # 2. List your apps
 
 # heroku apps
-
 # Pick the app that has the PostgreSQL add-on.
 
 # ⸻
@@ -20,9 +18,7 @@
 # 3. Get the database connection string
 
 # heroku config:get DATABASE_URL -a <your-app-name>
-
 # This returns a full Postgres connection string, something like:
-
 # postgres://username:password@host:5432/dbname
 
 
