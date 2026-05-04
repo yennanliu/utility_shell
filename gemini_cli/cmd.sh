db7e656ecc310eb178e2bc6dbdf295d8ff63a753
yennj12 <f339339@gmail.com>
2026-05-04 17:25:44 +0800

    Update cmd.sh

--- gemini_cli/cmd.sh
+++ gemini_cli/cmd.sh
@@ -1,4 +1,8 @@
 # 1. YOLO mode (similar as claude --dangerously-skip-permissions)
+# V1
 gemini --yolo
 # OR
 gemini --approval-mode=yolo
+
+# V2
+gemini --approval-mode=yolo --allowed-tools shell_exec,file_write,file_read,list_directory
