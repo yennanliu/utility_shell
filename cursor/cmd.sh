48bc4754c0d2142b0b251b4906c26297bf990f15
yennj12 <f339339@gmail.com>
2026-06-08 23:43:11 +0800

    Create cmd.sh

---
+++ cursor/cmd.sh
@@ -0,0 +1,6 @@
+# 1  reset zoom in / out at cursor IDE
+# To reset the zoom level in the Cursor IDE (or most other IDEs like VS Code and JetBrains) back to 100%, simply use the Reset Zoom keyboard shortcut:
+# https://www.google.com/search?q=how+to+reset+cursor+%60IDE+zoom+in+%2F+out+%3F&oq=how+to+reset+cursor+%60IDE+zoom++in+%2F+out+%3F&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRigATIHCAIQIRigATIHCAMQIRigATIHCAQQIRigATIHCAUQIRiPAjIHCAYQIRiPAtIBBzIxM2owajeoAgCwAgA&sourceid=chrome&ie=UTF-8
+
+# Mac: Press Cmd + 0
+# Cursor / VS Code Menu: Go to View > Appearance > Reset Zoom.
